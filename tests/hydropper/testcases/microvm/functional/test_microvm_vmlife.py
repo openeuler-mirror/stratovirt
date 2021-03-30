c47422e33762de52c5ec91e59e8a385484b6a1ad
Zhu huankai <zhuhuankai1@huawei.com>
2021-03-30 15:07:06 +0800

    tests/hydropper: add vmlife testcases
    vmlife testcases is used to test virtual machine's
    life, such as start, pause and cont.
    
    Signed-off-by: Zhu huankai <zhuhuankai1@huawei.com>
    Signed-off-by: Chen Qun <kuhn.chenqun@huawei.com>
    Signed-off-by: Pan Nengyuan <pannengyuan@huawei.com>

---
+++ tests/hydropper/testcases/microvm/functional/test_microvm_vmlife.py
@@ -0,0 +1,119 @@
+# Copyright (c) 2021 Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# StratoVirt is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan
+# PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#         http:#license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+"""Test microvm vmlife"""
+
+import logging
+import pytest
+from utils import utils_qmp
+from utils.config import CONFIG
+from utils.exception import QMPTimeoutError
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(filename='/var/log/pytest.log', level=logging.DEBUG, format=LOG_FORMAT)
+
+
+@pytest.mark.acceptance
+@pytest.mark.parametrize("vcpu_count, memsize, vnetnums",
+                         [(1, 256, 1),
+                          (2, 1024, 2),
+                          (8, 8192, 3),
+                          (32, 40960, 4)])
+def test_microvm_start(microvm, vcpu_count, memsize, vnetnums):
+    """Test a normal microvm start"""
+    if CONFIG.stratovirt_feature != "pci" and vnetnums > 2:
+        pytest.skip('It is not supportted if vnet number is greater than 2 in mmio.')
+    test_vm = microvm
+    test_vm.basic_config(vcpu_count=vcpu_count, mem_size=memsize * 1024 * 1024, vnetnums=vnetnums)
+    test_vm.launch()
+    vmhwinfo = test_vm.get_guest_hwinfo()
+    logging.debug("current vmhwinfo is %s", vmhwinfo)
+    assert vmhwinfo["cpu"]["vcpu_count"] == vcpu_count
+    assert vmhwinfo["mem"]["memsize"] > (memsize * 1024 * 90 / 100)
+    if CONFIG.stratovirt_feature == "pci":
+        assert len(vmhwinfo["virtio"]["virtio_blk"]) == 1
+        assert len(vmhwinfo["virtio"]["virtio_net"]) == vnetnums
+    else:
+        assert len(vmhwinfo["virtio"]["virtio_blk"]) == 6
+        assert len(vmhwinfo["virtio"]["virtio_net"]) == 2
+    # virtio_rng is not supported yet.
+    # assert len(vmhwinfo["virtio"]["virtio_rng"]) == vrngnums
+    assert len(vmhwinfo["virtio"]["virtio_console"]) == 1
+    test_vm.shutdown()
+
+
+@pytest.mark.system
+@pytest.mark.parametrize("destroy_value", [9, 15])
+def test_microvm_destroy(microvm, destroy_value):
+    """Test a normal microvm destroy(kill -9)"""
+    test_vm = microvm
+    test_vm.launch()
+    test_vm.destroy(signal=destroy_value)
+
+
+@pytest.mark.system
+def test_microvm_inshutdown(microvm):
+    """Test a normal microvm inshutdown"""
+    test_vm = microvm
+    test_vm.launch()
+    test_vm.inshutdown()
+
+
+@pytest.mark.acceptance
+def test_microvm_pause_resume(microvm):
+    """Test a normal microvm pause"""
+    test_vm = microvm
+    test_vm.launch()
+    resp = test_vm.query_status()
+    utils_qmp.assert_qmp(resp, "return/status", "running")
+    test_vm.stop()
+    test_vm.event_wait(name='STOP')
+    resp = test_vm.query_status()
+    utils_qmp.assert_qmp(resp, "return/status", "paused")
+    test_vm.cont()
+    test_vm.event_wait(name='RESUME')
+    resp = test_vm.query_status()
+    utils_qmp.assert_qmp(resp, "return/status", "running")
+    ret, _ = test_vm.serial_cmd("ls")
+    assert ret == 0
+
+
+@pytest.mark.system
+def test_microvm_pause_resume_abnormal(microvm):
+    """Abnormal test for microvm pause/resume"""
+    test_vm = microvm
+    test_vm.launch()
+    resp = test_vm.cont()
+    utils_qmp.assert_qmp(resp, "error/class", "GenericError")
+    with pytest.raises(QMPTimeoutError):
+        test_vm.event_wait(name='RESUME', timeout=3.0)
+    test_vm.qmp_reconnect()
+    ret, _ = test_vm.serial_cmd("ls")
+    assert ret == 0
+    resp = test_vm.stop()
+    utils_qmp.assert_qmp(resp, "return", {})
+    test_vm.event_wait(name='STOP')
+    resp = test_vm.stop()
+    utils_qmp.assert_qmp(resp, "error/class", "GenericError")
+    with pytest.raises(QMPTimeoutError):
+        test_vm.event_wait(name='STOP', timeout=3.0)
+    test_vm.qmp_reconnect()
+    resp = test_vm.cont()
+    utils_qmp.assert_qmp(resp, "return", {})
+    test_vm.event_wait(name='RESUME')
+    ret, _ = test_vm.serial_cmd("ls")
+    assert ret == 0
+    resp = test_vm.cont()
+    utils_qmp.assert_qmp(resp, "error/class", "GenericError")
+    ret, _ = test_vm.serial_cmd("ls")
+    assert ret == 0
+    with pytest.raises(QMPTimeoutError):
+        test_vm.event_wait(name='RESUME', timeout=3.0)
+    test_vm.qmp_reconnect()
