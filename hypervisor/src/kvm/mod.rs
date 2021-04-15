f0a8546eee8cd2b25916539b24bfa8fc4755978a
Xiaohe Yang <yangxiaohe2@huawei.com>
2021-04-15 19:13:44 +0800

    hypervisor: Add new crate for hypervisor APIs
    Store fds of KVM into the global variable. So we don't need to
    store them in structs or pass them as function parameters any
    more. Also more test cases can be run in the enviroment without
    /dev/kvm.
    This commit is a preparation for later usage.
    
    Signed-off-by: Jie Yang <yangjieyj.yang@huawei.com>

---
+++ hypervisor/src/kvm/mod.rs
@@ -0,0 +1,58 @@
+// Copyright (c) 2020 Huawei Technologies Co.,Ltd. All rights reserved.
+//
+// StratoVirt is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//         http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+use std::sync::Arc;
+
+use arc_swap::ArcSwap;
+use kvm_ioctls::{Kvm, VmFd};
+
+pub struct KVMFds {
+    pub fd: Option<Kvm>,
+    pub vm_fd: Option<VmFd>,
+}
+
+impl Default for KVMFds {
+    fn default() -> Self {
+        Self {
+            fd: None,
+            vm_fd: None,
+        }
+    }
+}
+
+impl KVMFds {
+    pub fn new() -> Self {
+        match Kvm::new() {
+            Ok(fd) => {
+                let vm_fd = match fd.create_vm() {
+                    Ok(vm_fd) => vm_fd,
+                    Err(e) => {
+                        error!("Failed to create VM in KVM: {}", e);
+                        return KVMFds::default();
+                    }
+                };
+                KVMFds {
+                    fd: Some(fd),
+                    vm_fd: Some(vm_fd),
+                }
+            }
+            Err(e) => {
+                error!("Failed to open /dev/kvm: {}", e);
+                KVMFds::default()
+            }
+        }
+    }
+}
+
+lazy_static! {
+    pub static ref KVM_FDS: ArcSwap<KVMFds> = ArcSwap::from(Arc::new(KVMFds::new()));
+}
