b788b8b78dc2b3039bfc77428ef442da1dc2dd1c
Wei Gao <gaowei66@huawei.com>
2021-05-28 14:44:51 +0800

    seccomp: update seccomp bpfrules for snapshot.
    Signed-off-by: Wei Gao <gaowei66@huawei.com>

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -19,7 +19,6 @@ extern crate machine_manager;
 #[cfg(target_arch = "x86_64")]
 #[macro_use]
 extern crate migration_derive;
-#[cfg(target_arch = "x86_64")]
 #[macro_use]
 extern crate vmm_sys_util;
 
