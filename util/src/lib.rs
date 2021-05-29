openeuler-mirror/stratovirt

commit 5930116d17ce5eb47d86a7cb95f1eb88c9f46f35

util: extract set_raw/canon_mode to util crate
When launching VM, we will set stdin to raw-mode. And when VM is destroyed,
we will recover stdin to canonical mode. If using `set_raw/conon_mode` function
of vmm-sys-util third-party crate, we can not input `Enter` in UEFI shell.

This commit fixes this problem by implementing seting/recovering terminal mode
in util crate. This commit also defers setting to raw-mode to realization of
PL011/serial devices.

In this commit we save the old mode of terminal in global variable, because we
change the terminal to raw mode when launching a VM and have to recover terminal
to old mode which is saved in global varible. This can gurantee that the mode of
terminal remains unchanged.

Signed-off-by: Xiaohe Yang <yangxiaohe2@huawei.com>

util/src/lib.rs
@@ -17,8 +17,8 @@ extern crate error_chain;
 extern crate vmm_sys_util;
 #[macro_use]
 extern crate log;
-extern crate kvm_bindings;
-extern crate kvm_ioctls;
+#[macro_use]
+extern crate lazy_static;
 
 pub mod aio;
 pub mod arg_parser;
@@ -133,4 +133,51 @@ pub mod errors {
             }
         }
     }
+}
+
+use libc::{tcgetattr, tcsetattr, termios, TCSANOW};
+use std::sync::{Arc, Mutex};
+use vmm_sys_util::terminal::Terminal;
+
+lazy_static! {
+    pub static ref TERMINAL_MODE: Arc<Mutex<Option<termios>>> = Arc::new(Mutex::new(None));
+}
+
+pub fn set_termi_raw_mode() -> std::io::Result<()> {
+    let tty_fd = std::io::stdin().lock().tty_fd();
+
+    // Safe because this only set the `old_term_mode` struct to zero.
+    let mut old_term_mode: termios = unsafe { std::mem::zeroed() };
+    // Safe because this only get stdin's current mode and save it.
+    let ret = unsafe { tcgetattr(tty_fd, &mut old_term_mode as *mut _) };
+    if ret < 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+    *TERMINAL_MODE.lock().unwrap() = Some(old_term_mode);
+
+    let mut new_term_mode: termios = old_term_mode;
+    // Safe because this function only change the `new_term_mode` argument.
+    unsafe { libc::cfmakeraw(&mut new_term_mode as *mut _) };
+    // Safe because this function only set the stdin to raw mode.
+    let ret = unsafe { tcsetattr(tty_fd, TCSANOW, &new_term_mode as *const _) };
+    if ret < 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(())
+}
+
+pub fn set_termi_canon_mode() -> std::io::Result<()> {
+    let tty_fd = std::io::stdin().lock().tty_fd();
+    if let Some(old_term_mode) = TERMINAL_MODE.lock().unwrap().as_ref() {
+        // Safe because this only recover the stdin's mode.
+        let ret = unsafe { tcsetattr(tty_fd, TCSANOW, old_term_mode as *const _) };
+        if ret < 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+    } else {
+        debug!("stdin's mode is not initialized: please check the config");
+    }
+
+    Ok(())
 }

devices/src/legacy/pl011.rs
@@ -25,6 +25,7 @@ use machine_manager::config::{BootSource, Param};
 use sysbus::{SysBus, SysBusDevOps, SysBusDevType, SysRes};
 use util::byte_code::ByteCode;
 use util::loop_context::{EventNotifier, EventNotifierHelper, NotifierOperation};
+use util::set_termi_raw_mode;
 use vmm_sys_util::epoll::EventSet;
 use vmm_sys_util::eventfd::EventFd;
 use vmm_sys_util::terminal::Terminal;
@@ -161,6 +162,7 @@ impl PL011 {
         region_size: u64,
         bs: &Arc<Mutex<BootSource>>,
     ) -> Result<Arc<Mutex<Self>>> {
+        set_termi_raw_mode().chain_err(|| "Failed to set terminal to raw mode")?;
         self.set_sys_resource(sysbus, region_base, region_size)
             .chain_err(|| "Failed to allocate system resource for PL011.")?;
 

devices/src/legacy/serial.rs
@@ -25,6 +25,7 @@ use hypervisor::KVM_FDS;
 use machine_manager::config::{BootSource, Param};
 use sysbus::{errors::Result as SysBusResult, SysBus, SysBusDevOps, SysBusDevType, SysRes};
 use util::loop_context::{EventNotifier, EventNotifierHelper, NotifierOperation};
+use util::set_termi_raw_mode;
 use vmm_sys_util::{epoll::EventSet, eventfd::EventFd, terminal::Terminal};
 
 use super::errors::{ErrorKind, Result};
@@ -113,6 +114,7 @@ impl Serial {
     ) -> Result<Arc<Mutex<Self>>> {
         use super::errors::ResultExt;
 
+        set_termi_raw_mode().chain_err(|| "Failed to set terminal to raw mode")?;
         self.output = Some(Box::new(std::io::stdout()));
         self.interrupt_evt = Some(EventFd::new(libc::EFD_NONBLOCK)?);
         self.set_sys_resource(sysbus, region_base, region_size)

machine/src/micro_vm/mod.rs
@@ -99,12 +99,12 @@ use util::device_tree;
 use util::device_tree::CompileFDT;
 use util::loop_context::{EventLoopManager, EventNotifierHelper};
 use util::seccomp::BpfRule;
+use util::set_termi_canon_mode;
 use virtio::{
     create_tap, qmp_balloon, qmp_query_balloon, Balloon, Block, Console, Net, Rng, VhostKern,
     VirtioDevice, VirtioMmioDevice,
 };
 use vmm_sys_util::eventfd::EventFd;
-use vmm_sys_util::terminal::Terminal;
 
 use super::{
     errors::{ErrorKind as MachineErrorKind, Result as MachineResult},
@@ -1299,12 +1299,9 @@ impl EventLoopManager for LightMachine {
     }
 
     fn loop_cleanup(&self) -> util::errors::Result<()> {
-        if let Err(e) = std::io::stdin().lock().set_canon_mode() {
-            error!(
-                "destroy virtual machine: reset stdin to canonical mode failed, {}",
-                e
-            );
-        }
+        use util::errors::ResultExt;
+
+        set_termi_canon_mode().chain_err(|| "Failed to set terminal to canonical mode")?;
         Ok(())
     }
 }

machine/src/standard_vm/aarch64/mod.rs
@@ -38,9 +38,9 @@ use util::byte_code::ByteCode;
 use util::device_tree::{self, CompileFDT};
 use util::loop_context::{EventLoopManager, EventNotifierHelper};
 use util::seccomp::BpfRule;
+use util::set_termi_canon_mode;
 use virtio::{qmp_balloon, qmp_query_balloon};
 use vmm_sys_util::eventfd::EventFd;
-use vmm_sys_util::terminal::Terminal;
 
 use super::{AcpiBuilder, StdMachineOps};
 use crate::errors::{ErrorKind, Result};
@@ -625,12 +625,9 @@ impl EventLoopManager for StdMachine {
     }
 
     fn loop_cleanup(&self) -> util::errors::Result<()> {
-        if let Err(e) = std::io::stdin().lock().set_canon_mode() {
-            error!(
-                "destroy virtual machine: reset stdin to canonical mode failed, {}",
-                e
-            );
-        }
+        use util::errors::ResultExt;
+
+        set_termi_canon_mode().chain_err(|| "Failed to set terminal to canonical mode")?;
         Ok(())
     }
 }

machine/src/standard_vm/x86_64/mod.rs
@@ -43,9 +43,9 @@ use pci::{PciDevOps, PciHost};
 use sysbus::SysBus;
 use util::loop_context::{EventLoopManager, EventNotifierHelper};
 use util::seccomp::BpfRule;
+use util::set_termi_canon_mode;
 use virtio::{qmp_balloon, qmp_query_balloon};
 use vmm_sys_util::eventfd::EventFd;
-use vmm_sys_util::terminal::Terminal;
 
 use super::errors::{ErrorKind, Result};
 use super::{AcpiBuilder, StdMachineOps};
@@ -699,12 +699,9 @@ impl EventLoopManager for StdMachine {
     }
 
     fn loop_cleanup(&self) -> util::errors::Result<()> {
-        if let Err(e) = std::io::stdin().lock().set_canon_mode() {
-            error!(
-                "destroy virtual machine: reset stdin to canonical mode failed, {}",
-                e
-            );
-        }
+        use util::errors::ResultExt;
+
+        set_termi_canon_mode().chain_err(|| "Failed to set terminal to canonical mode")?;
         Ok(())
     }
 }

machine_manager/src/qmp/mod.rs
@@ -42,7 +42,8 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use vmm_sys_util::terminal::Terminal;
+use util::leak_bucket::LeakBucket;
+use util::set_termi_canon_mode;
 
 use crate::event_loop::EventLoop;
 use crate::machine::MachineExternalInterface;
@@ -53,7 +54,6 @@ use crate::{
 };
 use qmp_schema as schema;
 use schema::QmpCommand;
-use util::leak_bucket::LeakBucket;
 
 static mut QMP_CHANNEL: Option<Arc<QmpChannel>> = None;
 
@@ -370,11 +370,8 @@ pub fn handle_qmp(
                 };
                 event!(SHUTDOWN; shutdown_msg);
                 TempCleaner::clean();
+                set_termi_canon_mode().expect("Failed to set terminal to canonical mode.");
 
-                std::io::stdin()
-                    .lock()
-                    .set_canon_mode()
-                    .expect("Failed to set terminal to canon mode.");
                 std::process::exit(0);
             }
 

machine_manager/src/signal_handler.rs
@@ -14,8 +14,8 @@ use crate::temp_cleaner::TempCleaner;
 use std::io::Write;
 
 use libc::{c_int, c_void, siginfo_t};
+use util::set_termi_canon_mode;
 use vmm_sys_util::signal::register_signal_handler;
-use vmm_sys_util::terminal::Terminal;
 
 pub const VM_EXIT_GENE_ERR: i32 = 1;
 const SYSTEMCALL_OFFSET: isize = 6;
@@ -26,10 +26,7 @@ fn basic_clean() {
     // clean temporary file
     TempCleaner::clean();
 
-    std::io::stdin()
-        .lock()
-        .set_canon_mode()
-        .expect("Failed to set terminal to canon mode.");
+    set_termi_canon_mode().expect("Failed to set terminal to canon mode.");
 }
 
 pub fn exit_with_code(code: i32) {

commit b77d22a2229d48ba380edb4a38ad05f7604887e1

util: fix log format error when printing log info to stdio
The bug is caused by commit 5930116d. When launching VM configuring `-D`, the
log is redirected to terminal. But the format of StratoVirt's log is abnormal,
and the next line always start at the end-offset of the previous line.
The log looks like follows,

```
INFO: Initrd is configured
                          INFO: vcpu0 is running
```

This problem is relavant to "OPOST" flag of terminal. This flag deactivates
default output processing, in which terminal will automatically insert a
<CR> after receiving a <LF>.

This commit adds this flag to disable the default processing of terminal and
let terminal just print received data.

Signed-off-by: Xiaohe Yang <yangxiaohe2@huawei.com>

util/src/lib.rs
@@ -135,7 +135,7 @@ pub mod errors {
     }
 }
 
-use libc::{tcgetattr, tcsetattr, termios, TCSANOW};
+use libc::{tcgetattr, tcsetattr, termios, OPOST, TCSANOW};
 use std::sync::{Arc, Mutex};
 use vmm_sys_util::terminal::Terminal;
 
@@ -158,6 +158,7 @@ pub fn set_termi_raw_mode() -> std::io::Result<()> {
     let mut new_term_mode: termios = old_term_mode;
     // Safe because this function only change the `new_term_mode` argument.
     unsafe { libc::cfmakeraw(&mut new_term_mode as *mut _) };
+    new_term_mode.c_oflag |= OPOST;
     // Safe because this function only set the stdin to raw mode.
     let ret = unsafe { tcsetattr(tty_fd, TCSANOW, &new_term_mode as *const _) };
     if ret < 0 {