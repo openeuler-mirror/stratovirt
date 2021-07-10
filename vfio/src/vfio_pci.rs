f83e7407d3dfdf42851939810e9ad9f47fb4b941
Xinle.Guo <guoxinle1@huawei.com>
2021-07-10 13:57:45 +0800

    vfio: add vfio pci device basic structure
    It needs to be provided to vitual machines as pci device.
    Create VfioPciDevice structure to save vfio device, MSI-X,
    pci config space and pci bar information.
    Export several data structures for save MSI-X, BAR, gsi
    and irqfds data
    
    Signed-off-by: Xinle.Guo <guoxinle1@huawei.com>

--- vfio/src/vfio_pci.rs
+++ vfio/src/vfio_pci.rs
@@ -9,3 +9,54 @@
 // KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
 // NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
+
+const PCI_NUM_BARS: u8 = 6;
+const PCI_ROM_SLOT: u8 = 6;
+
+struct MsixTable {
+    table_bar: u8,
+    table_offset: u64,
+    table_size: u64,
+}
+
+struct VfioMsixInfo {
+    // Table bar, table offset and table size info.
+    table: MsixTable,
+    // Msix enteries.
+    enteries: u16,
+    // Vfio device irq info
+    #[allow(dead_code)]
+    vfio_irq: HashMap<u32, VfioIrq>,
+}
+
+struct VfioBar {
+    vfio_region: VfioRegion,
+    region_type: RegionType,
+    size: u64,
+}
+
+struct GsiMsiRoute {
+    irq_fd: Option<EventFd>,
+    gsi: i32,
+}
+
+/// VfioPciDevice is a VFIO PCI device. It implements PciDevOps trait for a PCI device.
+/// And it is bound to a VFIO device.
+pub struct VfioPciDevice {
+    pci_config: PciConfig,
+    config_size: u64,
+    // Offset of pci config space region within vfio device fd.
+    config_offset: u64,
+    // Vfio device which is bound to.
+    vfio_device: Arc<VfioDevice>,
+    // Cache of MSI-X setup.
+    msix_info: Option<VfioMsixInfo>,
+    // Bars information without ROM.
+    vfio_bars: Arc<Mutex<Vec<VfioBar>>>,
+    // Maintains a list of GSI with irqfds that are registered to kvm.
+    gsi_msi_routes: Arc<Mutex<Vec<GsiMsiRoute>>>,
+    devfn: u8,
+    dev_id: u16,
+    name: String,
+    parent_bus: Weak<Mutex<PciBus>>,
+}
