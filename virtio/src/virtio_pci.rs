b1a69e3fb0841e6649ba330c2c8112227e4a8b92
Jiajie Li <lijiajie11@huawei.com>
2022-01-20 19:20:31 +0800

    pci: add get_dev_path for bootindex
    The boot sequence information of bootable devices in the firmware
    includes the priority and device path. The format of the device path
    is mostly parsed after IEEE 1275-1994, 3.2.1.1 "Node names" (a leading
    slash is expected and not returned):
    
        /driver-name@unit-address[:device-arguments][<LF>]
    
    Signed-off-by: Jiajie Li <lijiajie11@huawei.com>

--- virtio/src/virtio_pci.rs
+++ virtio/src/virtio_pci.rs
@@ -1123,6 +1123,21 @@ impl PciDevOps for VirtioPciDevice {
             .reset()
             .chain_err(|| "Fail to reset virtio device")
     }
+
+    fn get_dev_path(&self) -> Option<String> {
+        let parent_bus = self.parent_bus.upgrade().unwrap();
+        match self.device.lock().unwrap().device_type() {
+            VIRTIO_TYPE_BLOCK => {
+                // The virtio blk device is identified as a single-channel SCSI device,
+                // so add scsi controler indetification without channel, scsi-id and lun.
+                let parent_dev_path = self.get_parent_dev_path(parent_bus);
+                let mut dev_path = self.populate_dev_path(parent_dev_path, self.devfn, "/scsi@");
+                dev_path.push_str("/disk@0,0");
+                Some(dev_path)
+            }
+            _ => None,
+        }
+    }
 }
 
 impl StateTransfer for VirtioPciDevice {
