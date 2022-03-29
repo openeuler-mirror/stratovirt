e6a3bdfb9dcae6c9619f10dff94a92e026d9e1ba
Yan Wang <wangyan122@huawei.com>
2022-03-29 20:23:39 +0800

    vhost-user-net: implement the realize() and activate() function
    Implement the realize() and activate() function.
    
    Signed-off-by: Yan Wang <wangyan122@huawei.com>

--- virtio/src/vhost/user/net.rs
+++ virtio/src/vhost/user/net.rs
@@ -20,10 +20,16 @@ use util::byte_code::ByteCode;
 use util::num_ops::{read_u32, write_u32};
 use vmm_sys_util::eventfd::EventFd;
 
-use super::super::super::errors::{ErrorKind, Result};
+use super::super::super::errors::{ErrorKind, Result, ResultExt};
 use super::super::super::{
-    net::VirtioNetConfig, Queue, VirtioDevice, VirtioInterrupt, VIRTIO_TYPE_NET,
+    net::{build_device_config_space, VirtioNetConfig},
+    Queue, VirtioDevice, VirtioInterrupt, VIRTIO_F_RING_EVENT_IDX, VIRTIO_F_VERSION_1,
+    VIRTIO_NET_F_GUEST_CSUM, VIRTIO_NET_F_GUEST_TSO4, VIRTIO_NET_F_GUEST_UFO,
+    VIRTIO_NET_F_HOST_TSO4, VIRTIO_NET_F_HOST_UFO, VIRTIO_NET_F_MAC, VIRTIO_NET_F_MRG_RXBUF,
+    VIRTIO_TYPE_NET,
 };
+use super::super::VhostOps;
+use super::VhostUserClient;
 
 /// Number of virtqueues.
 const QUEUE_NUM_NET: usize = 2;
@@ -32,7 +38,6 @@ const QUEUE_SIZE_NET: u16 = 256;
 
 /// Network device structure.
 pub struct Net {
-    #[allow(dead_code)]
     /// Configuration of the vhost user network device.
     net_cfg: NetworkInterfaceConfig,
     /// Bit mask of features supported by the backend.
@@ -41,9 +46,10 @@ pub struct Net {
     driver_features: u64,
     /// Virtio net configurations.
     device_config: VirtioNetConfig,
-    #[allow(dead_code)]
     /// System address space.
     mem_space: Arc<AddressSpace>,
+    /// Vhost user client
+    client: Option<VhostUserClient>,
     /// The notifier events from host.
     call_events: Vec<EventFd>,
 }
@@ -56,6 +62,7 @@ impl Net {
             driver_features: 0_u64,
             device_config: VirtioNetConfig::default(),
             mem_space: mem_space.clone(),
+            client: None,
             call_events: Vec::<EventFd>::new(),
         }
     }
@@ -64,6 +71,40 @@ impl Net {
 impl VirtioDevice for Net {
     /// Realize vhost user network device.
     fn realize(&mut self) -> Result<()> {
+        let socket_path = self
+            .net_cfg
+            .socket_path
+            .as_ref()
+            .map(|path| path.to_string())
+            .chain_err(|| "vhost-user: socket path is not found")?;
+        let client = VhostUserClient::new(&self.mem_space, &socket_path, self.queue_num() as u64)
+            .chain_err(|| {
+            "Failed to create the client which communicates with the server for vhost-user net"
+        })?;
+        client
+            .add_event_notifier()
+            .chain_err(|| "Failed to add event nitifier for vhost-user net")?;
+
+        self.device_features = client
+            .get_features()
+            .chain_err(|| "Failed to get features for vhost-user net")?;
+
+        let features = 1 << VIRTIO_F_VERSION_1
+            | 1 << VIRTIO_NET_F_GUEST_CSUM
+            | 1 << VIRTIO_NET_F_GUEST_TSO4
+            | 1 << VIRTIO_NET_F_GUEST_UFO
+            | 1 << VIRTIO_NET_F_HOST_TSO4
+            | 1 << VIRTIO_NET_F_HOST_UFO
+            | 1 << VIRTIO_NET_F_MRG_RXBUF
+            | 1 << VIRTIO_F_RING_EVENT_IDX;
+        self.device_features &= features;
+
+        self.client = Some(client);
+
+        if let Some(mac) = &self.net_cfg.mac {
+            self.device_features |= build_device_config_space(&mut self.device_config, mac);
+        }
+
         Ok(())
     }
 
@@ -135,9 +176,86 @@ impl VirtioDevice for Net {
         &mut self,
         _mem_space: Arc<AddressSpace>,
         _interrupt_cb: Arc<VirtioInterrupt>,
-        _queues: &[Arc<Mutex<Queue>>],
-        _queue_evts: Vec<EventFd>,
+        queues: &[Arc<Mutex<Queue>>],
+        queue_evts: Vec<EventFd>,
     ) -> Result<()> {
+        let client = match &self.client {
+            None => return Err("Failed to get client for vhost-user net".into()),
+            Some(client_) => client_,
+        };
+
+        client
+            .set_owner()
+            .chain_err(|| "Failed to set owner for vhost-user net")?;
+
+        let features = self.driver_features & !(1 << VIRTIO_NET_F_MAC);
+        client
+            .set_features(features)
+            .chain_err(|| "Failed to set features for vhost-user net")?;
+
+        client
+            .set_mem_table()
+            .chain_err(|| "Failed to set mem table for vhost-user net")?;
+
+        for (queue_index, queue_mutex) in queues.iter().enumerate() {
+            let queue = queue_mutex.lock().unwrap();
+            let actual_size = queue.vring.actual_size();
+            let queue_config = queue.vring.get_queue_config();
+
+            client.set_vring_enable(queue_index, false).chain_err(|| {
+                format!(
+                    "Failed to set vring enable for vhost-user net, index: {}, false",
+                    queue_index,
+                )
+            })?;
+            client
+                .set_vring_num(queue_index, actual_size)
+                .chain_err(|| {
+                    format!(
+                        "Failed to set vring num for vhost-user net, index: {}, size: {}",
+                        queue_index, actual_size,
+                    )
+                })?;
+            client
+                .set_vring_addr(&queue_config, queue_index, 0)
+                .chain_err(|| {
+                    format!(
+                        "Failed to set vring addr for vhost-user net, index: {}",
+                        queue_index,
+                    )
+                })?;
+            client.set_vring_base(queue_index, 0).chain_err(|| {
+                format!(
+                    "Failed to set vring base for vhost-user net, index: {}",
+                    queue_index,
+                )
+            })?;
+            client
+                .set_vring_kick(queue_index, &queue_evts[queue_index])
+                .chain_err(|| {
+                    format!(
+                        "Failed to set vring kick for vhost-user net, index: {}",
+                        queue_index,
+                    )
+                })?;
+
+            client
+                .set_vring_call(queue_index, &self.call_events[queue_index])
+                .chain_err(|| {
+                    format!(
+                        "Failed to set vring call for vhost-user net, index: {}",
+                        queue_index,
+                    )
+                })?;
+
+            client.set_vring_enable(queue_index, true).chain_err(|| {
+                format!(
+                    "Failed to set vring enable for vhost-user net, index: {}",
+                    queue_index,
+                )
+            })?;
+        }
+
         Ok(())
     }
 
