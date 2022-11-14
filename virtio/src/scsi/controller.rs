f7f53aae0389669ab891849586dd743cf28dd72e
liuxiangdong <liuxiangdong5@huawei.com>
2022-11-14 13:06:49 +0800

    virtio-scsi: set max_target to 255 according to virtio spec
    Virtio Spec:
    max_target should be less than or equal to 255.
    
    Signed-off-by: liuxiangdong <liuxiangdong5@huawei.com>

--- virtio/src/scsi/controller.rs
+++ virtio/src/scsi/controller.rs
@@ -28,7 +28,7 @@ use crate::VirtioError;
 use address_space::{AddressSpace, GuestAddress};
 use log::{error, info};
 use machine_manager::{
-    config::{ConfigCheck, ScsiCntlrConfig},
+    config::{ConfigCheck, ScsiCntlrConfig, VIRTIO_SCSI_MAX_TARGET},
     event_loop::EventLoop,
 };
 use util::aio::{Aio, AioCb, AioCompleteFunc, Iovec};
@@ -167,6 +167,7 @@ impl VirtioDevice for ScsiCntlr {
         self.state.config_space.cmd_per_lun = 128;
         // seg_max: queue size - 2, 32 bit.
         self.state.config_space.seg_max = self.queue_size() as u32 - 2;
+        self.state.config_space.max_target = VIRTIO_SCSI_MAX_TARGET;
 
         self.state.device_features |= (1_u64 << VIRTIO_F_VERSION_1)
             | (1_u64 << VIRTIO_SCSI_F_HOTPLUG)
