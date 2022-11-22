9a563f6a5b194c89bbbaddb84d9224ba5740d060
Mingwang Li <limingwang@huawei.com>
2022-11-22 14:25:49 +0800

    XHCI: Eliminate the Devil's numbers
    The length of the Setup Stage TRB is always 8, and this field is in bits
    0-15 of 08-0BH.
    
    Signed-off-by: Mingwang Li <limingwang@huawei.com>

--- usb/src/xhci/xhci_ring.rs
+++ usb/src/xhci/xhci_ring.rs
@@ -37,6 +37,10 @@ pub const TRB_TR_CH: u32 = 1 << 4;
 pub const TRB_TR_IOC: u32 = 1 << 5;
 /// Immediate Data.
 pub const TRB_TR_IDT: u32 = 1 << 6;
+/// TRB Transfer Length Mask
+pub const TRB_TR_LEN_MASK: u32 = 0x1ffff;
+/// Setup Stage TRB Length always 8
+pub const SETUP_TRB_TR_LEN: u32 = 8;
 
 const TRB_LINK_LIMIT: u32 = 32;
 /// The max size of a ring segment in bytes is 64k.
