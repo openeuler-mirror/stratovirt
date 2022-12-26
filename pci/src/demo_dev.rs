eca634f45eee37eaefed323e2bc9a5aa313af9cb
Zhang Bo <oscar.zhangbo@huawei.com>
2022-12-26 18:54:41 +0800

    demo: add a new demo device
    DemoDev is a demo PCIe device, that has 2 bars, 1 for msix, 1 for data handling.

---
+++ pci/src/demo_dev.rs
@@ -0,0 +1,146 @@
+/// DemoDev is a demo PCIe device, that has 2 bars, 1 for msix, 1 for data handling.
+/// 1. its functionality is to print heximal values that the guest writes,
+///    and do nothing if the guest reads its device memory.
+/// 2. After printing, it sends back a msix interrupt to the guest, which
+/// means that it has also msix capability. We assume msix bar is in bar 0.
+/// 3. Finally, it supports hotplug/hotunplug.
+/// As that it has device memory, it means it has a bar space, we assume the
+/// bar size is 4KB in bar 1.
+///
+/// The cmdline for the device is: -device pcie-demo-dev,addr=0x5,bus=pcie.0,id=demo0
+use std::{
+    sync::Mutex,
+    sync::{
+        atomic::{AtomicU16, Ordering},
+        Arc, Weak,
+    },
+};
+
+use address_space::Region;
+use anyhow::Ok;
+
+use crate::{
+    config::{
+        PciConfig, RegionType, DEVICE_ID, HEADER_TYPE, HEADER_TYPE_ENDPOINT,
+        PCIE_CONFIG_SPACE_SIZE, SUB_CLASS_CODE, VENDOR_ID,
+    },
+    init_msix, le_write_u16, PciBus, PciDevOps,
+};
+pub use anyhow::{bail, Result};
+
+//bar[0] for msix and bar[1] for dev mmio mem
+const DEMO_DEV_BAR_NUM: u8 = 2;
+
+pub struct DemoDev {
+    name: String,
+    config: PciConfig,
+    mem_region: Region,
+    devfn: u8,
+    parent_bus: Weak<Mutex<PciBus>>,
+    dev_id: Arc<AtomicU16>,
+}
+
+impl DemoDev {
+    pub fn new(name: String, devfn: u8, parent_bus: Weak<Mutex<PciBus>>) -> Self {
+        DemoDev {
+            name,
+            config: PciConfig::new(PCIE_CONFIG_SPACE_SIZE, DEMO_DEV_BAR_NUM),
+            mem_region: Region::init_container_region(u64::max_value() >> 10),
+            devfn,
+            parent_bus,
+            dev_id: Arc::new(AtomicU16::new(0)),
+        }
+    }
+}
+
+// reference to https://pci-ids.ucw.cz/read/PC?restrict=1
+// "DEAD BEEF" seems will not be used for a long time.
+const VENDOR_ID_DEMO: u16 = 0xDEAD;
+const DEVICE_ID_DEMO: u16 = 0xBEEF;
+// reference to https://pci-ids.ucw.cz/read/PD/
+const CLASS_CODE_DEMO: u16 = 0xEE;
+
+impl PciDevOps for DemoDev {
+    fn init_write_mask(&mut self) -> Result<()> {
+        self.config.init_common_write_mask()
+    }
+
+    fn init_write_clear_mask(&mut self) -> Result<()> {
+        self.config.init_common_write_clear_mask()
+    }
+
+    /// Realize PCI/PCIe device.
+    fn realize(mut self) -> Result<()> {
+        self.init_write_mask()?;
+        self.init_write_clear_mask()?;
+
+        let config = &mut self.config.config;
+        le_write_u16(config, DEVICE_ID as usize, DEVICE_ID_DEMO)?;
+        le_write_u16(config, VENDOR_ID as usize, VENDOR_ID_DEMO)?;
+        le_write_u16(config, SUB_CLASS_CODE as usize, CLASS_CODE_DEMO)?;
+        config[HEADER_TYPE as usize] = HEADER_TYPE_ENDPOINT;
+
+        let region = Region::init_container_region(0x1000);
+        self.config
+            .register_bar(1, region, RegionType::Mem64Bit, false, 0x1000)?;
+        init_msix(
+            0,
+            1,
+            &mut self.config,
+            self.dev_id.clone(),
+            &self.name,
+            None,
+            None,
+        )?;
+
+        let parent_bus = self.parent_bus.upgrade().unwrap();
+        let mut locked_parent_bus = parent_bus.lock().unwrap();
+        if locked_parent_bus.devices.get(&self.devfn).is_some() {
+            bail!("device already existed");
+        }
+        let devfn = self.devfn;
+        let demo_pci_dev = Arc::new(Mutex::new(self));
+        locked_parent_bus.devices.insert(devfn, demo_pci_dev);
+
+        Ok(())
+    }
+
+    /// Unrealize PCI/PCIe device.
+    fn unrealize(&mut self) -> Result<()> {
+        bail!("Unrealize of the demo device is not implemented yet");
+    }
+
+    /// read the pci configuration space
+    fn read_config(&mut self, offset: usize, data: &mut [u8]) {
+        self.config.read(offset, data);
+    }
+
+    /// write the pci configuration space
+    fn write_config(&mut self, offset: usize, data: &[u8]) {
+        let parent_bus = self.parent_bus.upgrade().unwrap();
+        let locked_parent_bus = parent_bus.lock().unwrap();
+
+        self.config.write(
+            offset,
+            data,
+            self.dev_id.load(Ordering::Acquire),
+            #[cfg(target_arch = "x86_64")]
+            None,
+            Some(&locked_parent_bus.mem_region),
+        );
+    }
+
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    /// Reset device
+    fn reset(&mut self, _reset_child_device: bool) -> Result<()> {
+        self.config.reset_common_regs()
+    }
+
+    /// Get device devfn
+    fn devfn(&self) -> Option<u8> {
+        Some(self.devfn)
+    }
+}
