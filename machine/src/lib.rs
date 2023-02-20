7ed6886f1cd3a846519ef8c4ca26d8e68e86a447
Zhang Bo <oscar.zhangbo@huawei.com>
2023-02-20 17:20:28 +0800

    demo_dev: add the data handling bar
    If the mmio address is writen, multiply the data[0] with 2. The data is
    reserved in a hashmap that's inside this device. The data will be removed
    after reading, as that we think it becomes useless after the test process
    has validated the data.
    Meanwhile, rm init_bars() because bars should be registered at the time
    that we register region_ops.

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -1289,7 +1289,8 @@ pub trait MachineOps {
         let demo_cfg = parse_demo_dev(vm_config, cfg_args.to_string())
             .with_context(|| "failed to parse cmdline for demo dev.")?;
 
-        let demo_dev = DemoDev::new(demo_cfg, devfn, parent_bus);
+        let sys_mem = self.get_sys_mem().clone();
+        let demo_dev = DemoDev::new(demo_cfg, devfn, sys_mem, parent_bus);
 
         demo_dev.realize()
     }
