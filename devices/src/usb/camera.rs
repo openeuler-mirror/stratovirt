a12f891890cd0ea9dd7f4f30681677deb98b7ad8
Zhang Bo <oscar.zhangbo@huawei.com>
2023-03-29 13:51:19 +0800

    camera: support handle usb control
    Signed-off-by: Zhang Bo <oscar.zhangbo@huawei.com>

--- devices/src/usb/camera.rs
+++ devices/src/usb/camera.rs
@@ -13,13 +13,27 @@
 //! Emulated camera device that based on UVC(USB video class) protocol.
 
 use anyhow::{bail, Result};
-use machine_manager::config::{CamBackendType, UsbCameraConfig};
+use log::{debug, error};
+use once_cell::sync::Lazy;
+
 use std::sync::{Arc, Mutex, Weak};
+use strum::EnumCount;
+use strum_macros::{EnumCount as EnumCountMacro, EnumIter};
 
 use super::config::USB_SPEED_HIGH;
 use super::xhci::xhci_controller::XhciDevice;
 use crate::camera_backend::{v4l2::V4l2HostDev, CameraHostdevOps};
-use crate::usb::{UsbDevice, UsbDeviceOps, UsbDeviceRequest, UsbEndpoint, UsbPacket};
+use crate::usb::config::*;
+use crate::usb::descriptor::*;
+use crate::usb::{
+    UsbDevice, UsbDeviceOps, UsbDeviceRequest, UsbEndpoint, UsbPacket, UsbPacketStatus,
+};
+use machine_manager::config::{CamBackendType, UsbCameraConfig};
+
+// CRC16 of "STRATOVIRT"
+const UVC_VENDOR_ID: u16 = 0xB74C;
+// The first 4 chars of "VIDEO", 5 substitutes V.
+const UVC_PRODUCT_ID: u16 = 0x51DE;
 
 #[allow(dead_code)]
 pub struct UsbCamera {
@@ -32,6 +46,74 @@ pub struct UsbCamera {
     backend_path: String,                       // backend interface file, eg. /dev/video0
     hostdev: Option<Box<dyn CameraHostdevOps>>, // backend device, eg. v4l2, demo, etc.
 }
+
+#[derive(Debug, EnumCountMacro, EnumIter)]
+enum UsbCameraStringIDs {
+    Invalid = 0,
+    Manufacture,
+    Product,
+    SerialNumber,
+    Configuration,
+    Iad,
+    VideoControl,
+    InputTerminal,
+    OutputTerminal,
+    SelectUnit,
+    ProcessingUnit,
+    VideoStreaming,
+}
+
+const UVC_CAMERA_STRINGS: [&str; UsbCameraStringIDs::COUNT] = [
+    "",
+    "StratoVirt",
+    "USB Camera",
+    "1",
+    "USB Camera Configuration",
+    "USB Camera Interface Associated Description",
+    "Video Control",
+    "Input Terminal",
+    "Output Terminal",
+    "Select Unit",
+    "Processing Unit",
+    "Video Streaming",
+];
+
+/// UVC Camera device descriptor
+static DESC_DEVICE_CAMERA: Lazy<Arc<UsbDescDevice>> = Lazy::new(|| {
+    Arc::new(UsbDescDevice {
+        device_desc: UsbDeviceDescriptor {
+            bLength: USB_DT_DEVICE_SIZE,
+            bDescriptorType: USB_DT_DEVICE,
+            idVendor: UVC_VENDOR_ID,
+            idProduct: UVC_PRODUCT_ID,
+            bcdDevice: 0,
+            iManufacturer: UsbCameraStringIDs::Manufacture as u8,
+            iProduct: UsbCameraStringIDs::Product as u8,
+            iSerialNumber: UsbCameraStringIDs::SerialNumber as u8,
+            bcdUSB: 0x0200, // TODO: support 3.0 Super
+            bDeviceClass: USB_CLASS_MISCELLANEOUS,
+            // Refer to https://www.usb.org/defined-class-codes for details.
+            bDeviceSubClass: 2,
+            bDeviceProtocol: 1, // Interface Association
+            bMaxPacketSize0: 64,
+            bNumConfigurations: 1,
+        },
+        configs: vec![Arc::new(UsbDescConfig {
+            config_desc: UsbConfigDescriptor {
+                bLength: USB_DT_CONFIG_SIZE,
+                bDescriptorType: USB_DT_CONFIGURATION,
+                wTotalLength: 0,
+                bNumInterfaces: 2,
+                bConfigurationValue: 1,
+                iConfiguration: UsbCameraStringIDs::Configuration as u8,
+                bmAttributes: USB_CONFIGURATION_ATTR_ONE | USB_CONFIGURATION_ATTR_SELF_POWER,
+                bMaxPower: 50,
+            },
+            // TODO: Add IAD descriptor, including VC&VS
+            interfaces: vec![],
+        })],
+    })
+});
 
 #[allow(dead_code)]
 impl UsbCamera {
@@ -53,7 +135,10 @@ impl UsbCamera {
 
         self.usb_device.reset_usb_endpoint();
         self.usb_device.speed = USB_SPEED_HIGH;
-        //TODO: init descriptor.
+        let s = UVC_CAMERA_STRINGS.iter().map(|&s| s.to_string()).collect();
+        self.usb_device
+            .init_descriptor(DESC_DEVICE_CAMERA.clone(), s)?;
+        // TODO: add device qualifier table.
         let camera = Arc::new(Mutex::new(self));
 
         Ok(camera)
@@ -96,7 +181,25 @@ impl Default for UsbCamera {
 impl UsbDeviceOps for UsbCamera {
     fn reset(&mut self) {}
 
-    fn handle_control(&mut self, _packet: &mut UsbPacket, _device_req: &UsbDeviceRequest) {}
+    fn handle_control(&mut self, packet: &mut UsbPacket, device_req: &UsbDeviceRequest) {
+        debug!("Into camera handle_control");
+        match self
+            .usb_device
+            .handle_control_for_descriptor(packet, device_req)
+        {
+            Ok(handled) => {
+                if handled {
+                    debug!("Camera control handled by descriptor, return directly.");
+                } else {
+                    error!("Camera: unhandled control msg: {}", device_req.request_type);
+                }
+            }
+            Err(e) => {
+                error!("Camera descriptor error {:?}", e);
+                packet.status = UsbPacketStatus::Stall;
+            }
+        }
+    }
 
     fn handle_data(&mut self, _p: &mut UsbPacket) {}
 
