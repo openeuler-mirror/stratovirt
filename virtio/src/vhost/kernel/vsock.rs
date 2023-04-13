7ccf51a6a4e6413197cd7fc14ba4e5d76be09978
mayunlong <mayunlong6@huawei.com>
2023-04-13 11:25:29 +0800

    vhost-vsock: use irqfd in vhost-vsock
    Signed-off-by: mayunlong<mayunlong6@huawei.com>

--- virtio/src/vhost/kernel/vsock.rs
+++ virtio/src/vhost/kernel/vsock.rs
@@ -21,15 +21,14 @@ use vmm_sys_util::ioctl::ioctl_with_ref;
 use address_space::AddressSpace;
 use byteorder::{ByteOrder, LittleEndian};
 use machine_manager::config::{VsockConfig, DEFAULT_VIRTQUEUE_SIZE};
-use machine_manager::event_loop::{register_event_helper, unregister_event_helper};
+use machine_manager::event_loop::unregister_event_helper;
 use migration::{DeviceStateDesc, FieldDesc, MigrationHook, MigrationManager, StateTransfer};
 use migration_derive::{ByteCode, Desc};
 use util::byte_code::ByteCode;
-use util::loop_context::EventNotifierHelper;
 use util::num_ops::read_u32;
 
-use super::super::{VhostNotify, VhostOps};
-use super::{VhostBackend, VhostIoHandler, VHOST_VSOCK_SET_GUEST_CID, VHOST_VSOCK_SET_RUNNING};
+use super::super::VhostOps;
+use super::{VhostBackend, VHOST_VSOCK_SET_GUEST_CID, VHOST_VSOCK_SET_RUNNING};
 use crate::{
     Queue, VirtioDevice, VirtioError, VirtioInterrupt, VirtioInterruptType, VIRTIO_TYPE_VSOCK,
 };
@@ -105,6 +104,8 @@ pub struct Vsock {
     deactivate_evts: Vec<RawFd>,
     /// Device is broken or not.
     broken: Arc<AtomicBool>,
+    /// Save irqfd used for vhost-vsock
+    call_events: Vec<Arc<EventFd>>,
 }
 
 impl Vsock {
@@ -118,6 +119,7 @@ impl Vsock {
             interrupt_cb: None,
             deactivate_evts: Vec::new(),
             broken: Arc::new(AtomicBool::new(false)),
+            call_events: Vec::new(),
         }
     }
 
@@ -243,6 +245,14 @@ impl VirtioDevice for Vsock {
         Ok(())
     }
 
+    fn set_guest_notifiers(&mut self, queue_evts: &[Arc<EventFd>]) -> Result<()> {
+        for fd in queue_evts.iter() {
+            self.call_events.push(fd.clone());
+        }
+
+        Ok(())
+    }
+
     /// Activate the virtio device, this function is called by vcpu thread when frontend
     /// virtio driver is ready and write `DRIVER_OK` to backend.
     fn activate(
@@ -253,7 +263,6 @@ impl VirtioDevice for Vsock {
         queue_evts: Vec<Arc<EventFd>>,
     ) -> Result<()> {
         let cid = self.vsock_cfg.guest_cid;
-        let mut host_notifies = Vec::new();
         // The receive queue and transmit queue will be handled in vhost.
         let vhost_queues = queues[..2].to_vec();
         // This event queue will be handled.
@@ -299,38 +308,25 @@ impl VirtioDevice for Vsock {
                 })?;
             drop(queue);
 
-            let host_notify = VhostNotify {
-                notify_evt: Arc::new(
-                    EventFd::new(libc::EFD_NONBLOCK).with_context(|| VirtioError::EventFdCreate)?,
-                ),
-                queue: queue_mutex.clone(),
-            };
             backend
-                .set_vring_call(queue_index, host_notify.notify_evt.clone())
+                .set_vring_call(queue_index, self.call_events[queue_index].clone())
                 .with_context(|| {
                     format!("Failed to set vring call for vsock, index: {}", queue_index)
                 })?;
-            host_notifies.push(host_notify);
         }
 
         backend.set_guest_cid(cid)?;
         backend.set_running(true)?;
 
-        let handler = VhostIoHandler {
-            interrupt_cb,
-            host_notifies,
-            device_broken: self.broken.clone(),
-        };
-
-        let notifiers = EventNotifierHelper::internal_notifiers(Arc::new(Mutex::new(handler)));
-        register_event_helper(notifiers, None, &mut self.deactivate_evts)?;
         self.broken.store(false, Ordering::SeqCst);
 
         Ok(())
     }
 
     fn deactivate(&mut self) -> Result<()> {
-        unregister_event_helper(None, &mut self.deactivate_evts)
+        unregister_event_helper(None, &mut self.deactivate_evts)?;
+        self.call_events.clear();
+        Ok(())
     }
 
     fn reset(&mut self) -> Result<()> {
