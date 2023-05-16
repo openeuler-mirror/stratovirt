0ce5b8d2df6d19be7f6f3b69979c449abbbecbe7
yexiao <yexiao7@huawei.com>
2023-05-16 19:54:02 +0800

    VNC: Modify Demo device.
    Fix vnc mst test_switch_display_device.
    The demo gpu device used for mst needs to update the registered process in ui console
    and add a field to identify the unique device.
    
    Signed-off-by: Xiao Ye <yexiao7@huawei.com>

--- pci/src/demo_device/gpu_device.rs
+++ pci/src/demo_device/gpu_device.rs
@@ -63,6 +63,7 @@ impl From<u8> for GpuEvent {
 }
 
 pub struct DemoGpu {
+    dev_id: String,
     sys_mem: Arc<AddressSpace>,
     con: Option<Weak<Mutex<DisplayConsole>>>,
     width: u32,
@@ -73,8 +74,9 @@ pub struct DemoGpu {
 unsafe impl Send for DemoGpu {}
 
 impl DemoGpu {
-    pub fn new(sys_mem: Arc<AddressSpace>) -> Self {
+    pub fn new(sys_mem: Arc<AddressSpace>, dev_id: String) -> Self {
         Self {
+            dev_id,
             sys_mem,
             con: None,
             width: 0,
@@ -212,7 +214,7 @@ impl DeviceTypeOperation for DemoGpu {
 
     fn realize(&mut self) -> Result<()> {
         let con_opts = Arc::new(HwOpts {});
-        self.con = console_init("demo-gpu".to_string(), ConsoleType::Graphic, con_opts);
+        self.con = console_init(self.dev_id.clone(), ConsoleType::Graphic, con_opts);
 
         // Create Image.
         self.width = 640;
