9951ce30c4b45e8d62fa6e2767af92a75b5660e8
yezengruan <yezengruan@huawei.com>
2023-08-04 09:46:58 +0800

    add copyright to the head of some files
    Some files lack copyright, let's add it.
    
    Signed-off-by: yezengruan <yezengruan@huawei.com>

--- pci/src/demo_device/kbd_pointer_device.rs
+++ pci/src/demo_device/kbd_pointer_device.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Huawei Technologies Co.,Ltd. All rights reserved.
+//
+// StratoVirt is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan
 // PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
