openeuler-mirror/stratovirt

commit 721a74a3d68de60e51987080c3f756eddd7723fe

vitio: Remove redundant pub declarations
Signed-off-by: jiewangqun <jiewangqun@huawei.com>

virtio/src/device/gpu.rs
@@ -280,7 +280,7 @@ struct VirtioGpuResourceDetachBacking {
 
 impl ByteCode for VirtioGpuResourceDetachBacking {}
 
-pub struct GpuOpts {
+struct GpuOpts {
     /// Status of the emulated physical outputs.
     output_states: Arc<Mutex<[VirtioGpuOutputState; VIRTIO_GPU_MAX_OUTPUTS]>>,
     /// Config space of the GPU device.
@@ -488,15 +488,15 @@ fn create_surface(
 }
 
 // simple formats for fbcon/X use
-pub const VIRTIO_GPU_FORMAT_B8G8R8A8_UNORM: u32 = 1;
-pub const VIRTIO_GPU_FORMAT_B8G8R8X8_UNORM: u32 = 2;
-pub const VIRTIO_GPU_FORMAT_A8R8G8B8_UNORM: u32 = 3;
-pub const VIRTIO_GPU_FORMAT_X8R8G8B8_UNORM: u32 = 4;
-pub const VIRTIO_GPU_FORMAT_R8G8B8A8_UNORM: u32 = 67;
-pub const VIRTIO_GPU_FORMAT_X8B8G8R8_UNORM: u32 = 68;
-pub const VIRTIO_GPU_FORMAT_A8B8G8R8_UNORM: u32 = 121;
-pub const VIRTIO_GPU_FORMAT_R8G8B8X8_UNORM: u32 = 134;
-pub const VIRTIO_GPU_FORMAT_INVALID_UNORM: u32 = 135;
+const VIRTIO_GPU_FORMAT_B8G8R8A8_UNORM: u32 = 1;
+const VIRTIO_GPU_FORMAT_B8G8R8X8_UNORM: u32 = 2;
+const VIRTIO_GPU_FORMAT_A8R8G8B8_UNORM: u32 = 3;
+const VIRTIO_GPU_FORMAT_X8R8G8B8_UNORM: u32 = 4;
+const VIRTIO_GPU_FORMAT_R8G8B8A8_UNORM: u32 = 67;
+const VIRTIO_GPU_FORMAT_X8B8G8R8_UNORM: u32 = 68;
+const VIRTIO_GPU_FORMAT_A8B8G8R8_UNORM: u32 = 121;
+const VIRTIO_GPU_FORMAT_R8G8B8X8_UNORM: u32 = 134;
+const _VIRTIO_GPU_FORMAT_INVALID_UNORM: u32 = 135;
 
 pub fn get_pixman_format(format: u32) -> Result<pixman_format_code_t> {
     match format {
@@ -1416,7 +1416,7 @@ impl EventNotifierHelper for GpuIoHandler {
 }
 
 #[derive(Clone, Copy, Debug, ByteCode)]
-pub struct VirtioGpuConfig {
+struct VirtioGpuConfig {
     events_read: u32,
     events_clear: u32,
     num_scanouts: u32,

vhost_user_fs/src/main.rs
@@ -42,7 +42,7 @@ use util::arg_parser::ArgMatches;
 use util::{arg_parser, logger, seccomp::SeccompOpt};
 
 #[derive(Error, Debug)]
-pub enum MainError {
+enum MainError {
     #[error("VhostUserFs")]
     VhostUserFs {
         #[from]
@@ -60,7 +60,7 @@ pub enum MainError {
     },
 }
 
-pub trait ExitCode {
+trait ExitCode {
     /// Returns the value to use as the exit status.
     fn code(self) -> i32;
 }

virtio/src/device/balloon.rs
@@ -88,19 +88,19 @@ struct BalloonStat {
 #[allow(dead_code)]
 struct VirtioBalloonConfig {
     /// The target page numbers of balloon device.
-    pub num_pages: u32,
+    num_pages: u32,
     /// Number of pages we've actually got in balloon device.
-    pub actual: u32,
-    pub _reserved: u32,
-    pub _reserved1: u32,
+    actual: u32,
+    _reserved: u32,
+    _reserved1: u32,
     /// Buffer percent is a percentage of memory actually needed by
     /// the applications and services running inside the virtual machine.
     /// This parameter takes effect only when VIRTIO_BALLOON_F_MESSAGE_VQ is supported.
     /// Recommended value range: [20, 80] and default is 50.
-    pub membuf_percent: u32,
+    membuf_percent: u32,
     /// Monitor interval(second) host wants to adjust VM memory size.
     /// Recommended value range: [5, 300] and default is 10.
-    pub monitor_interval: u32,
+    monitor_interval: u32,
 }
 
 impl ByteCode for BalloonStat {}
@@ -950,7 +950,7 @@ impl Balloon {
     /// # Argument
     ///
     /// * `size` - Target memory size.
-    pub fn set_guest_memory_size(&mut self, size: u64) -> Result<()> {
+    fn set_guest_memory_size(&mut self, size: u64) -> Result<()> {
         let host_page_size = host_page_size();
         if host_page_size > BALLOON_PAGE_SIZE && !self.mem_info.lock().unwrap().has_huge_page() {
             warn!("Balloon used with backing page size > 4kiB, this may not be reliable");
@@ -976,10 +976,11 @@ impl Balloon {
     }
 
     /// Get the actual memory size of guest.
-    pub fn get_guest_memory_size(&self) -> u64 {
+    fn get_guest_memory_size(&self) -> u64 {
         self.mem_info.lock().unwrap().get_ram_size() - self.get_balloon_memory_size()
     }
-    pub fn set_num_pages(&mut self, target: u32) {
+
+    fn set_num_pages(&mut self, target: u32) {
         self.num_pages = target;
     }
 }

virtio/src/device/block.rs
@@ -475,9 +475,9 @@ struct BlockIoHandler {
     /// The block backend opened by the block device.
     block_backend: Option<Arc<Mutex<dyn BlockDriverOps<AioCompleteCb>>>>,
     /// The align requirement of request(offset/len).
-    pub req_align: u32,
+    req_align: u32,
     /// The align requirement of buffer(iova_base).
-    pub buf_align: u32,
+    buf_align: u32,
     /// The number of sectors of the disk image.
     disk_sectors: u64,
     /// Serial number of the block device.

virtio/src/device/net.rs
@@ -113,7 +113,7 @@ pub struct VirtioNetConfig {
 impl ByteCode for VirtioNetConfig {}
 
 /// The control mode used for packet receive filtering.
-pub struct CtrlRxMode {
+struct CtrlRxMode {
     /// If the device should receive all incoming packets.
     promisc: bool,
     /// If the device should allow all incoming multicast packets.
@@ -145,7 +145,7 @@ impl Default for CtrlRxMode {
 
 #[derive(Default, Clone)]
 struct MacAddress {
-    pub address: [u8; MAC_ADDR_LEN],
+    address: [u8; MAC_ADDR_LEN],
 }
 
 /// The Mac information used to filter incoming packet.

virtio/src/device/scsi_cntlr.rs
@@ -353,7 +353,7 @@ impl ByteCode for VirtioScsiCtrlAnResp {}
 
 #[repr(C, packed)]
 #[derive(Default, Clone, Copy)]
-pub struct VirtioScsiCmdReq {
+struct VirtioScsiCmdReq {
     /// Logical Unit Number.
     lun: [u8; 8],
     /// Command identifier.
@@ -370,7 +370,7 @@ impl ByteCode for VirtioScsiCmdReq {}
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct VirtioScsiCmdResp {
+struct VirtioScsiCmdResp {
     /// Sense data length.
     sense_len: u32,
     /// Resudual bytes in data buffer.
@@ -566,7 +566,7 @@ impl<T: Clone + ByteCode + Default, U: Clone + ByteCode + Default> VirtioScsiReq
     }
 }
 
-pub struct ScsiCtrlQueueHandler {
+struct ScsiCtrlQueueHandler {
     /// The ctrl virtqueue.
     queue: Arc<Mutex<Queue>>,
     /// EventFd for the ctrl virtqueue.
@@ -675,7 +675,7 @@ impl EventNotifierHelper for ScsiCtrlQueueHandler {
     }
 }
 
-pub struct ScsiEventQueueHandler {
+struct ScsiEventQueueHandler {
     /// The Event virtqueue.
     _queue: Arc<Mutex<Queue>>,
     /// EventFd for the Event virtqueue.
@@ -739,7 +739,7 @@ fn virtio_scsi_get_lun_id(lun: [u8; 8]) -> u16 {
     (((lun[2] as u16) << 8) | (lun[3] as u16)) & 0x3FFF
 }
 
-pub struct ScsiCmdQueueHandler {
+struct ScsiCmdQueueHandler {
     /// The scsi controller.
     scsibus: Arc<Mutex<ScsiBus>>,
     /// The Cmd virtqueue.

virtio/src/device/serial.rs
@@ -103,7 +103,7 @@ impl ByteCode for VirtioConsoleConfig {}
 
 impl VirtioConsoleConfig {
     /// Create configuration of virtio-serial devices.
-    pub fn new(max_nr_ports: u32) -> Self {
+    fn new(max_nr_ports: u32) -> Self {
         VirtioConsoleConfig {
             cols: 0_u16,
             rows: 0_u16,

virtio/src/queue/split.rs
@@ -116,11 +116,11 @@ impl QueueConfig {
         }
     }
 
-    pub fn get_desc_size(&self) -> u64 {
+    fn get_desc_size(&self) -> u64 {
         min(self.size, self.max_size) as u64 * DESCRIPTOR_LEN
     }
 
-    pub fn get_used_size(&self, features: u64) -> u64 {
+    fn get_used_size(&self, features: u64) -> u64 {
         let size = if virtio_has_feature(features, VIRTIO_F_RING_EVENT_IDX) {
             2_u64
         } else {
@@ -130,7 +130,7 @@ impl QueueConfig {
         size + VRING_FLAGS_AND_IDX_LEN + (min(self.size, self.max_size) as u64) * USEDELEM_LEN
     }
 
-    pub fn get_avail_size(&self, features: u64) -> u64 {
+    fn get_avail_size(&self, features: u64) -> u64 {
         let size = if virtio_has_feature(features, VIRTIO_F_RING_EVENT_IDX) {
             2_u64
         } else {

commit 2b3822668810e8ddcda040423553b548d0d3904d

fix mod test unresolved import error
mod test error: unresolved import `virtio::VIRTIO_GPU_FORMAT_INVALID_UNORM`
Fixes: 721a74a3d68d ("vitio: Remove redundant pub declarations")

Signed-off-by: yezengruan <yezengruan@huawei.com>

virtio/src/device/gpu.rs
@@ -496,7 +496,7 @@ const VIRTIO_GPU_FORMAT_R8G8B8A8_UNORM: u32 = 67;
 const VIRTIO_GPU_FORMAT_X8B8G8R8_UNORM: u32 = 68;
 const VIRTIO_GPU_FORMAT_A8B8G8R8_UNORM: u32 = 121;
 const VIRTIO_GPU_FORMAT_R8G8B8X8_UNORM: u32 = 134;
-const _VIRTIO_GPU_FORMAT_INVALID_UNORM: u32 = 135;
+pub const VIRTIO_GPU_FORMAT_INVALID_UNORM: u32 = 135;
 
 pub fn get_pixman_format(format: u32) -> Result<pixman_format_code_t> {
     match format {