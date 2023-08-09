86457f85322618c473e47fb374440dc241fce19e
mayunlong <mayunlong6@huawei.com>
2023-08-09 22:56:04 +0800

    file: change lock to fcntl()
    use fcntl() to lock file instead of flock().
    
    Signed-off-by: mayunlong<mayunlong6@huawei.com>

--- util/src/file.rs
+++ util/src/file.rs
@@ -19,6 +19,16 @@ use anyhow::{bail, Context, Ok, Result};
 
 const MIN_FILE_ALIGN: u32 = 512;
 const MAX_FILE_ALIGN: u32 = 4096;
+/// Permission to read
+const FILE_LOCK_READ: u64 = 0x01;
+/// Permission to write
+const FILE_LOCK_WRITE: u64 = 0x02;
+/// All permissions
+const FILE_LOCK_ALL:[u64; 2] = [FILE_LOCK_READ, FILE_LOCK_WRITE];
+/// Permission lock base address, consistent with qemu
+const LOCK_PERM_BASE: u64 = 100;
+/// Shared lock base address, consistent with qemu
+const LOCK_SHARED_BASE: u64 = 200;
 
 pub fn open_file(path: &str, read_only: bool, direct: bool) -> Result<File> {
     let mut options = OpenOptions::new();
@@ -94,39 +104,73 @@ pub fn get_file_alignment(file: &File, direct: bool) -> (u32, u32) {
     (req_align, buf_align)
 }
 
-pub fn lock_file(file: &File, path: &str, read_only: bool) -> Result<()> {
-    let (lockop, lockname) = if read_only {
-        (libc::LOCK_SH | libc::LOCK_NB, "read lock")
-    } else {
-        (libc::LOCK_EX | libc::LOCK_NB, "write lock")
-    };
+fn do_fcntl_lock(
+    file: &File,
+    path: &str,
+    lockname: &str,
+    flock: libc::flock,
+    is_lock: bool,
+) -> Result<()> {
     // SAFETY: the file has a valid raw fd.
-    let ret = unsafe { libc::flock(file.as_raw_fd(), lockop) };
-    if ret < 0 {
+    let ret = unsafe { libc::fcntl(file.as_raw_fd(), libc::F_SETLK, &flock) };
+    if ret >= 0 {
+        return Ok(());
+    }
+
+    if is_lock {
         bail!(
             "Failed to get {} on file: {}. Is it used more than once or \
-            another process using the same file?. Error: {}",
+            another process using the same file? Error: {}",
             lockname,
             path,
             std::io::Error::last_os_error(),
         );
-    }
-
-    Ok(())
-}
-
-pub fn unlock_file(file: &File, path: &str) -> Result<()> {
-    // SAFETY: the file has a valid raw fd.
-    let ret = unsafe { libc::flock(file.as_raw_fd(), libc::LOCK_UN) };
-    if ret < 0 {
+    } else {
         bail!(
             "Failed to release lock on file: {}. Error: {}",
             path,
             std::io::Error::last_os_error(),
         );
     }
+}
+
+fn lock_or_unlock_file(
+    file: &File,
+    path: &str,
+    lock_op: i16,
+    lock_name: &str,
+    is_lock: bool,
+) -> Result<()> {
+    let pid = unsafe { libc::getpid() };
+    let mut flock = libc::flock {
+        l_whence: libc::SEEK_SET as i16,
+        l_len: 1,
+        l_pid: pid,
+        l_type: lock_op,
+        l_start: 0,
+    };
+
+    for lock in FILE_LOCK_ALL {
+        flock.l_start = (LOCK_PERM_BASE + lock) as i64;
+        do_fcntl_lock(file, path, lock_name, flock, is_lock)?;
+    }
+    flock.l_start = (LOCK_SHARED_BASE + FILE_LOCK_WRITE) as i64;
+    do_fcntl_lock(file, path, lock_name, flock, is_lock)?;
 
     Ok(())
+}
+
+pub fn lock_file(file: &File, path: &str, read_only: bool) -> Result<()> {
+    let (lock_op, lock_name) = if read_only {
+        (libc::F_RDLCK, "read lock")
+    } else {
+        (libc::F_WRLCK, "write lock")
+    };
+    lock_or_unlock_file(file, path, lock_op as i16, lock_name, true)
+}
+
+pub fn unlock_file(file: &File, path: &str) -> Result<()> {
+    lock_or_unlock_file(file, path, libc::F_UNLCK as i16, "", false)
 }
 
 pub fn clear_file(path: String) -> Result<()> {
