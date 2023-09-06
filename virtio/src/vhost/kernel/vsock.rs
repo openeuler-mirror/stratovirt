19756ef4a56339e5a79e561776bddf552aa7a76d
Yan Wang <wangyan122@huawei.com>
2023-09-06 15:31:31 +0800

    vsock: diable VIRTIO_F_ACCESS_PLATFORM feature
    Currently, we don't support VIRTIO_F_ACCESS_PLATFORM feature.
    So, disable it.
    
    Signed-off-by: Yan Wang <wangyan122@huawei.com>

--- virtio/src/vhost/kernel/vsock.rs
+++ virtio/src/vhost/kernel/vsock.rs
@@ -23,7 +23,7 @@ use super::super::{VhostNotify, VhostOps};
 use super::{VhostBackend, VhostIoHandler, VHOST_VSOCK_SET_GUEST_CID, VHOST_VSOCK_SET_RUNNING};
 use crate::{
     check_config_space_rw, Queue, VirtioBase, VirtioDevice, VirtioError, VirtioInterrupt,
-    VirtioInterruptType, VIRTIO_TYPE_VSOCK,
+    VirtioInterruptType, VIRTIO_F_ACCESS_PLATFORM, VIRTIO_TYPE_VSOCK,
 };
 use address_space::AddressSpace;
 use machine_manager::config::{VsockConfig, DEFAULT_VIRTQUEUE_SIZE};
@@ -194,9 +194,10 @@ impl VirtioDevice for Vsock {
 
     fn init_config_features(&mut self) -> Result<()> {
         let backend = self.backend.as_ref().unwrap();
-        self.base.device_features = backend
+        let features = backend
             .get_features()
             .with_context(|| "Failed to get features for vsock")?;
+        self.base.device_features = features & !(1_u64 << VIRTIO_F_ACCESS_PLATFORM);
         Ok(())
     }
 
