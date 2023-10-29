57e2bd5a6dd9c68e1dd93c4e6e38a594fba56026
Mingwang Li <limingwang@huawei.com>
2023-10-29 21:03:29 +0800

    fdt: fdt implements normalization
    Signed-off-by: Mingwang Li <limingwang@huawei.com>

--- machine/src/micro_vm/mod.rs
+++ machine/src/micro_vm/mod.rs
@@ -53,16 +53,12 @@ use crate::MachineBase;
 use address_space::{AddressSpace, GuestAddress, Region};
 #[cfg(target_arch = "x86_64")]
 use cpu::CPUBootConfig;
-#[cfg(target_arch = "aarch64")]
-use cpu::PMU_INTR;
 use cpu::{CPUTopology, CpuLifecycleState};
 #[cfg(target_arch = "x86_64")]
 use devices::legacy::FwCfgOps;
 #[cfg(target_arch = "aarch64")]
 use devices::legacy::PL031;
 use devices::sysbus::{SysBus, IRQ_BASE, IRQ_MAX};
-#[cfg(target_arch = "aarch64")]
-use devices::sysbus::{SysBusDevType, SysRes};
 #[cfg(target_arch = "aarch64")]
 use devices::{ICGICConfig, ICGICv2Config, ICGICv3Config, InterruptController, GIC_IRQ_MAX};
 #[cfg(target_arch = "x86_64")]
@@ -1286,199 +1282,18 @@ impl EventLoopManager for LightMachine {
     }
 }
 
-// Function that helps to generate serial node in device-tree.
-//
-// # Arguments
-//
-// * `dev_info` - Device resource info of serial device.
-// * `fdt` - Flatted device-tree blob where serial node will be filled into.
-#[cfg(target_arch = "aarch64")]
-fn generate_serial_device_node(fdt: &mut FdtBuilder, res: &SysRes) -> util::Result<()> {
-    let node = format!("pl011@{:x}", res.region_base);
-    let serial_node_dep = fdt.begin_node(&node)?;
-    fdt.set_property_string("compatible", "arm,pl011\0arm,primecell")?;
-    fdt.set_property_string("clock-names", "uartclk\0apb_pclk")?;
-    fdt.set_property_array_u32(
-        "clocks",
-        &[device_tree::CLK_PHANDLE, device_tree::CLK_PHANDLE],
-    )?;
-    fdt.set_property_array_u64("reg", &[res.region_base, res.region_size])?;
-    fdt.set_property_array_u32(
-        "interrupts",
-        &[
-            device_tree::GIC_FDT_IRQ_TYPE_SPI,
-            res.irq as u32,
-            device_tree::IRQ_TYPE_EDGE_RISING,
-        ],
-    )?;
-    fdt.end_node(serial_node_dep)?;
-
-    Ok(())
-}
-
-// Function that helps to generate RTC node in device-tree.
-//
-// # Arguments
-//
-// * `dev_info` - Device resource info of RTC device.
-// * `fdt` - Flatted device-tree blob where RTC node will be filled into.
-#[cfg(target_arch = "aarch64")]
-fn generate_rtc_device_node(fdt: &mut FdtBuilder, res: &SysRes) -> util::Result<()> {
-    let node = format!("pl031@{:x}", res.region_base);
-    let rtc_node_dep = fdt.begin_node(&node)?;
-    fdt.set_property_string("compatible", "arm,pl031\0arm,primecell\0")?;
-    fdt.set_property_string("clock-names", "apb_pclk")?;
-    fdt.set_property_u32("clocks", device_tree::CLK_PHANDLE)?;
-    fdt.set_property_array_u64("reg", &[res.region_base, res.region_size])?;
-    fdt.set_property_array_u32(
-        "interrupts",
-        &[
-            device_tree::GIC_FDT_IRQ_TYPE_SPI,
-            res.irq as u32,
-            device_tree::IRQ_TYPE_LEVEL_HIGH,
-        ],
-    )?;
-    fdt.end_node(rtc_node_dep)?;
-
-    Ok(())
-}
-
-// Function that helps to generate Virtio-Mmio device's node in device-tree.
-//
-// # Arguments
-//
-// * `dev_info` - Device resource info of Virtio-Mmio device.
-// * `fdt` - Flatted device-tree blob where node will be filled into.
-#[cfg(target_arch = "aarch64")]
-fn generate_virtio_devices_node(fdt: &mut FdtBuilder, res: &SysRes) -> util::Result<()> {
-    let node = format!("virtio_mmio@{:x}", res.region_base);
-    let virtio_node_dep = fdt.begin_node(&node)?;
-    fdt.set_property_string("compatible", "virtio,mmio")?;
-    fdt.set_property_u32("interrupt-parent", device_tree::GIC_PHANDLE)?;
-    fdt.set_property_array_u64("reg", &[res.region_base, res.region_size])?;
-    fdt.set_property_array_u32(
-        "interrupts",
-        &[
-            device_tree::GIC_FDT_IRQ_TYPE_SPI,
-            res.irq as u32,
-            device_tree::IRQ_TYPE_EDGE_RISING,
-        ],
-    )?;
-    fdt.end_node(virtio_node_dep)?;
-    Ok(())
-}
-
-#[cfg(target_arch = "aarch64")]
-fn generate_pmu_node(fdt: &mut FdtBuilder) -> util::Result<()> {
-    let node = "pmu";
-    let pmu_node_dep = fdt.begin_node(node)?;
-    fdt.set_property_string("compatible", "arm,armv8-pmuv3")?;
-    fdt.set_property_u32("interrupt-parent", device_tree::GIC_PHANDLE)?;
-    fdt.set_property_array_u32(
-        "interrupts",
-        &[
-            device_tree::GIC_FDT_IRQ_TYPE_PPI,
-            PMU_INTR,
-            device_tree::IRQ_TYPE_LEVEL_HIGH,
-        ],
-    )?;
-
-    fdt.end_node(pmu_node_dep)?;
-    Ok(())
-}
-
 /// Trait that helps to generate all nodes in device-tree.
 #[allow(clippy::upper_case_acronyms)]
 #[cfg(target_arch = "aarch64")]
 trait CompileFDTHelper {
-    /// Function that helps to generate cpu nodes.
-    fn generate_cpu_nodes(&self, fdt: &mut FdtBuilder) -> util::Result<()>;
     /// Function that helps to generate memory nodes.
     fn generate_memory_node(&self, fdt: &mut FdtBuilder) -> util::Result<()>;
-    /// Function that helps to generate Virtio-mmio devices' nodes.
-    fn generate_devices_node(&self, fdt: &mut FdtBuilder) -> util::Result<()>;
     /// Function that helps to generate the chosen node.
     fn generate_chosen_node(&self, fdt: &mut FdtBuilder) -> util::Result<()>;
 }
 
 #[cfg(target_arch = "aarch64")]
 impl CompileFDTHelper for LightMachine {
-    fn generate_cpu_nodes(&self, fdt: &mut FdtBuilder) -> util::Result<()> {
-        let node = "cpus";
-
-        let cpus_node_dep = fdt.begin_node(node)?;
-        fdt.set_property_u32("#address-cells", 0x02)?;
-        fdt.set_property_u32("#size-cells", 0x0)?;
-
-        // Generate CPU topology
-        let cpu_map_node_dep = fdt.begin_node("cpu-map")?;
-        for socket in 0..self.base.cpu_topo.sockets {
-            let sock_name = format!("cluster{}", socket);
-            let sock_node_dep = fdt.begin_node(&sock_name)?;
-            for cluster in 0..self.base.cpu_topo.clusters {
-                let clster = format!("cluster{}", cluster);
-                let cluster_node_dep = fdt.begin_node(&clster)?;
-
-                for core in 0..self.base.cpu_topo.cores {
-                    let core_name = format!("core{}", core);
-                    let core_node_dep = fdt.begin_node(&core_name)?;
-
-                    for thread in 0..self.base.cpu_topo.threads {
-                        let thread_name = format!("thread{}", thread);
-                        let thread_node_dep = fdt.begin_node(&thread_name)?;
-                        let vcpuid = self.base.cpu_topo.threads
-                            * self.base.cpu_topo.cores
-                            * self.base.cpu_topo.clusters
-                            * socket
-                            + self.base.cpu_topo.threads * self.base.cpu_topo.cores * cluster
-                            + self.base.cpu_topo.threads * core
-                            + thread;
-                        fdt.set_property_u32(
-                            "cpu",
-                            u32::from(vcpuid) + device_tree::CPU_PHANDLE_START,
-                        )?;
-                        fdt.end_node(thread_node_dep)?;
-                    }
-                    fdt.end_node(core_node_dep)?;
-                }
-                fdt.end_node(cluster_node_dep)?;
-            }
-            fdt.end_node(sock_node_dep)?;
-        }
-        fdt.end_node(cpu_map_node_dep)?;
-
-        for cpu_index in 0..self.base.cpu_topo.nrcpus {
-            let mpidr = self.base.cpus[cpu_index as usize]
-                .arch()
-                .lock()
-                .unwrap()
-                .mpidr();
-
-            let node = format!("cpu@{:x}", mpidr);
-            let mpidr_node_dep = fdt.begin_node(&node)?;
-            fdt.set_property_u32(
-                "phandle",
-                u32::from(cpu_index) + device_tree::CPU_PHANDLE_START,
-            )?;
-            fdt.set_property_string("device_type", "cpu")?;
-            fdt.set_property_string("compatible", "arm,arm-v8")?;
-            if self.base.cpu_topo.max_cpus > 1 {
-                fdt.set_property_string("enable-method", "psci")?;
-            }
-            fdt.set_property_u64("reg", mpidr & 0x007F_FFFF)?;
-            fdt.end_node(mpidr_node_dep)?;
-        }
-
-        fdt.end_node(cpus_node_dep)?;
-
-        // CPU Features : PMU
-        if self.base.cpu_features.pmu {
-            generate_pmu_node(fdt)?;
-        }
-
-        Ok(())
-    }
-
     fn generate_memory_node(&self, fdt: &mut FdtBuilder) -> util::Result<()> {
         let mem_base = MEM_LAYOUT[LayoutEntryType::Mem as usize].0;
         let mem_size = self.base.sys_mem.memory_end_address().raw_value()
@@ -1487,56 +1302,7 @@ impl CompileFDTHelper for LightMachine {
         let memory_node_dep = fdt.begin_node(node)?;
         fdt.set_property_string("device_type", "memory")?;
         fdt.set_property_array_u64("reg", &[mem_base, mem_size])?;
-        fdt.end_node(memory_node_dep)?;
-
-        Ok(())
-    }
-
-    fn generate_devices_node(&self, fdt: &mut FdtBuilder) -> util::Result<()> {
-        // timer
-        let mut cells: Vec<u32> = Vec::new();
-        for &irq in [13, 14, 11, 10].iter() {
-            cells.push(device_tree::GIC_FDT_IRQ_TYPE_PPI);
-            cells.push(irq);
-            cells.push(device_tree::IRQ_TYPE_LEVEL_HIGH);
-        }
-        let node = "timer";
-        let timer_node_dep = fdt.begin_node(node)?;
-        fdt.set_property_string("compatible", "arm,armv8-timer")?;
-        fdt.set_property("always-on", &Vec::new())?;
-        fdt.set_property_array_u32("interrupts", &cells)?;
-        fdt.end_node(timer_node_dep)?;
-
-        // clock
-        let node = "apb-pclk";
-        let clock_node_dep = fdt.begin_node(node)?;
-        fdt.set_property_string("compatible", "fixed-clock")?;
-        fdt.set_property_string("clock-output-names", "clk24mhz")?;
-        fdt.set_property_u32("#clock-cells", 0x0)?;
-        fdt.set_property_u32("clock-frequency", 24_000_000)?;
-        fdt.set_property_u32("phandle", device_tree::CLK_PHANDLE)?;
-        fdt.end_node(clock_node_dep)?;
-
-        // psci
-        let node = "psci";
-        let psci_node_dep = fdt.begin_node(node)?;
-        fdt.set_property_string("compatible", "arm,psci-0.2")?;
-        fdt.set_property_string("method", "hvc")?;
-        fdt.end_node(psci_node_dep)?;
-
-        for dev in self.base.sysbus.devices.iter() {
-            let locked_dev = dev.lock().unwrap();
-            let dev_type = locked_dev.sysbusdev_base().dev_type;
-            let sys_res = locked_dev.sysbusdev_base().res;
-            match dev_type {
-                SysBusDevType::PL011 => generate_serial_device_node(fdt, &sys_res)?,
-                SysBusDevType::Rtc => generate_rtc_device_node(fdt, &sys_res)?,
-                SysBusDevType::VirtioMmio => generate_virtio_devices_node(fdt, &sys_res)?,
-                _ => (),
-            }
-        }
-
-        Ok(())
+        fdt.end_node(memory_node_dep)
     }
 
     fn generate_chosen_node(&self, fdt: &mut FdtBuilder) -> util::Result<()> {
@@ -1558,9 +1324,7 @@ impl CompileFDTHelper for LightMachine {
             }
             None => {}
         }
-        fdt.end_node(chosen_node_dep)?;
-
-        Ok(())
+        fdt.end_node(chosen_node_dep)
     }
 }
 
@@ -1568,26 +1332,10 @@ impl CompileFDTHelper for LightMachine {
 impl device_tree::CompileFDT for LightMachine {
     fn generate_fdt_node(&self, fdt: &mut FdtBuilder) -> util::Result<()> {
         let node_dep = fdt.begin_node("")?;
-
-        fdt.set_property_string("compatible", "linux,dummy-virt")?;
-        fdt.set_property_u32("#address-cells", 0x2)?;
-        fdt.set_property_u32("#size-cells", 0x2)?;
-        fdt.set_property_u32("interrupt-parent", device_tree::GIC_PHANDLE)?;
-
-        self.generate_cpu_nodes(fdt)?;
+        self.base.generate_fdt_node(fdt)?;
         self.generate_memory_node(fdt)?;
-        self.generate_devices_node(fdt)?;
         self.generate_chosen_node(fdt)?;
-        // SAFETY: ARM architecture must have interrupt controllers in user mode.
-        self.base
-            .irq_chip
-            .as_ref()
-            .unwrap()
-            .generate_fdt_node(fdt)?;
-
-        fdt.end_node(node_dep)?;
-
-        Ok(())
+        fdt.end_node(node_dep)
     }
 }
 
