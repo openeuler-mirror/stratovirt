f07371a43c819387c4848dd060647f57fe34b52b
dehengli <lidh11@chinatelecom.cn>
2023-11-28 14:17:30 +0800

    tests: add cpu hotplug test
    Enable x86_64 mod_test and add cpu hotplug test.
    
    Signed-off-by: dehengli <lidh11@chinatelecom.cn>

--- tests/mod_test/tests/x86_64/mod.rs
+++ tests/mod_test/tests/x86_64/mod.rs
@@ -1,3 +1,4 @@
+// Copyright (c) 2023 China Telecom Co.,Ltd. All rights reserved.
 // Copyright (c) 2023 Huawei Technologies Co.,Ltd. All rights reserved.
 //
 // StratoVirt is licensed under Mulan PSL v2.
@@ -10,24 +11,4 @@
 // NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-#[cfg(target_arch = "aarch64")]
-mod aarch64;
-mod balloon_test;
-mod block_test;
-mod fwcfg_test;
-mod memory_test;
-mod net_test;
-mod pci_test;
-mod rng_test;
-mod scream_test;
-mod scsi_test;
-mod serial_test;
-mod usb_camera_test;
-mod usb_storage_test;
-mod usb_test;
-mod virtio_gpu_test;
-mod virtio_test;
-mod virtiofs_test;
-mod vnc_test;
-#[cfg(target_arch = "x86_64")]
-mod x86_64;
+mod cpu_hotplug_test;
