3503978e45aeb2e47641a6110bf676e302a82708
goriainovstanislav <goriainov.stanislav@huawei-partners.com>
2024-02-16 12:37:02 +0300

    xhci: Add streams to doorbell ops
    Deduce stream id from doorbell value and pass it to kick_endpoint().
    
    Signed-off-by: Stanislav Goriainov <goriainov.stanislav@huawei-partners.com>

--- devices/src/usb/xhci/xhci_regs.rs
+++ devices/src/usb/xhci/xhci_regs.rs
@@ -103,6 +103,9 @@ const XHCI_INTR_REG_SHIFT: u64 = 5;
 /// Doorbell Register Bit Field.
 /// DB Target.
 const DB_TARGET_MASK: u32 = 0xff;
+/// DB Stream.
+const DB_STREAM_ID_SHIFT: u32 = 16;
+const DB_STREAM_ID_MASK: u32 = 0xffff;
 /// Port Registers.
 const XHCI_PORTSC: u64 = 0x0;
 const XHCI_PORTPMSC: u64 = 0x4;
@@ -703,7 +706,8 @@ pub fn build_doorbell_ops(xhci_dev: &Arc<Mutex<XhciDevice>>) -> RegionOps {
             return false;
         } else {
             let ep_id = value & DB_TARGET_MASK;
-            if let Err(e) = xhci.kick_endpoint(slot_id, ep_id, 0) {
+            let stream_id = (value >> DB_STREAM_ID_SHIFT) & DB_STREAM_ID_MASK;
+            if let Err(e) = xhci.kick_endpoint(slot_id, ep_id, stream_id) {
                 error!("Failed to kick endpoint: {:?}", e);
                 xhci.host_controller_error();
                 return false;
