884dba97f5124d69690d644e9677af9cb4aaca3e
Jinhao Gao <gaojinhao@huawei.com>
2024-03-20 12:45:33 +0800

    Trace: Fix a clippy error
    Fix a clippy error
    
    Signed-off-by: Jinhao Gao <gaojinhao@huawei.com>

--- devices/src/pci/msix.rs
+++ devices/src/pci/msix.rs
@@ -450,7 +450,7 @@ impl Msix {
 
         let masked: bool = self.is_func_masked(config);
         let enabled: bool = self.is_enabled(config);
-        trace::msix_write_config(self.dev_id.load(Ordering::Relaxed) as u16, masked, enabled);
+        trace::msix_write_config(self.dev_id.load(Ordering::Relaxed), masked, enabled);
 
         let mask_state_changed = !((self.func_masked == masked) && (self.enabled == enabled));
 
