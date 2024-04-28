7d71547fdd0298a8689f40d6518ee80f3d1addd6
liuxiangdong <liuxiangdong5@huawei.com>
2024-04-28 12:18:51 +0800

    virtio-fs: use clone instead of reference to avoid deadlock
    `root_bus` will be locked for using its reference and then
    deadlock will occur in `add_virtio_pci_device`->`get_devfn_and_parent_bus`.
    
    (gdb) bt
      #0 syscall () at ../sysdeps/unix/sysv/linux/aarch64/syscall.S:38
      #1 0x0000aaaabb7588f4 in std::sys::unix::futex::futex_wait () at library/std/src/sys/unix/futex.rs:62
      #2 0x0000aaaababc3d44 in std::sys::unix::locks::futex_mutex::Mutex::lock_contended () at library/std/src/sys/unix/locks/futex_mutex.rs:61
      #3 0x0000aaaabb048420 in std::sys::unix::locks::futex_mutex::Mutex::lock (self=0xaaaac1825080)
      at /rustc/a55dd71d5fb0ec5a6a3a9e8c27b2127ba491ce52/library/std/src/sys/unix/locks/futex_mutex.rs:33
      #4 0x0000aaaabaf9b910 in std::sys_common::mutex::MovableMutex::raw_lock (self=0xaaaac1825080)
      at /rustc/a55dd71d5fb0ec5a6a3a9e8c27b2127ba491ce52/library/std/src/sys_common/mutex.rs:75
      #5 0x0000aaaabaf1c9a8 in std::sync::mutex::Mutexdevices::pci::bus::PciBus::lockdevices::pci::bus::PciBus (self=0xaaaac1825080)
      at /rustc/a55dd71d5fb0ec5a6a3a9e8c27b2127ba491ce52/library/std/src/sync/mutex.rs:266
      #6 0x0000aaaabaed0c90 in devices::pci::bus::PciBus::find_bus_by_name (bus=0xffffe9972688, bus_name=...) at devices/src/pci/bus.rs:147
      #7 0x0000aaaabac13dd8 in machine::MachineOps::get_devfn_and_parent_busmachine::aarch64::standard::StdMachine
      (self=0xaaaac1828fe8, bdf=0xffffe9973670) at machine/src/lib.rs:1357
      #8 0x0000aaaabac14494 in machine::MachineOps::add_virtio_pci_devicemachine::aarch64::standard::StdMachine
      (self=0xaaaac1828fe8, id=..., bdf=0xffffe9973670, device=..., multi_func=false, need_irqfd=false) at machine/src/lib.rs:1396
      #9 0x0000aaaabac0fa58 in machine::MachineOps::add_virtio_fsmachine::aarch64::standard::StdMachine
      (self=0xaaaac1828fe8, vm_config=0xffffe9977658, cfg_args=...) at machine/src/lib.rs:875
      #10 0x0000aaaabac184a4 in machine::MachineOps::add_devicesmachine::aarch64::standard::StdMachine
      (self=0xaaaac1828fe8, vm_config=0xffffe9977658) at machine/src/lib.rs:1787
      #11 0x0000aaaababf4618 in machine::aarch64::standard::{impl#2}::realize (vm=0xffffe9975bf0, vm_config=0xffffe9977658)
      at machine/src/aarch64/standard.rs:631
      #12 0x0000aaaababdff80 in stratovirt::real_main (cmd_args=0xffffe9977430, vm_config=0xffffe9977658) at src/main.rs:174
      #13 0x0000aaaababdf4a4 in stratovirt::run () at src/main.rs:107
      #14 0x0000aaaababdeefc in stratovirt::main () at src/main.rs:62
    
    Fix: 6079887d8c4b(virtio-fs: don't trigger irq of the virtio-fs device in test hypervisor)
    
    Signed-off-by: liuxiangdong <liuxiangdong5@huawei.com>

--- machine/src/lib.rs
+++ machine/src/lib.rs
@@ -870,7 +870,7 @@ pub trait MachineOps {
                 let bdf = get_pci_bdf(cfg_args)?;
                 let multi_func = get_multi_function(cfg_args)?;
                 let root_bus = self.get_pci_host()?.lock().unwrap().root_bus.clone();
-                let msi_irq_manager = &root_bus.lock().unwrap().msi_irq_manager;
+                let msi_irq_manager = root_bus.lock().unwrap().msi_irq_manager.clone();
                 let need_irqfd = msi_irq_manager.as_ref().unwrap().irqfd_enable();
                 self.add_virtio_pci_device(&id_clone, &bdf, device, multi_func, need_irqfd)
                     .with_context(|| "Failed to add pci fs device")?;
