2472e3feb6faa32d114f09e9465bce0be0c8128f
liuxiangdong <liuxiangdong5@huawei.com>
2024-05-20 12:19:17 +0800

    display: use clap to parse the parameters of the display config
    Use clap to parse the parameters of the display config
    
    Signed-off-by: liuxiangdong <liuxiangdong5@huawei.com>

--- machine_manager/src/config/display.rs
+++ machine_manager/src/config/display.rs
@@ -13,17 +13,15 @@
 #[cfg(feature = "gtk")]
 use std::sync::Arc;
 
+use anyhow::Result;
 #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-use anyhow::Context;
-use anyhow::{bail, Result};
+use anyhow::{bail, Context};
+use clap::{ArgAction, Parser};
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "gtk")]
 use vmm_sys_util::eventfd::EventFd;
 
-use crate::config::{CmdParser, ExBool, VmConfig};
-
-#[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-static DEFAULT_UI_PATH: &str = "/tmp/";
+use crate::config::{parse_bool, str_slip_to_clap, VmConfig};
 
 /// Event fd related to power button in gtk.
 #[cfg(feature = "gtk")]
@@ -41,88 +39,59 @@ pub struct UiContext {
 }
 
 #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct OhuiConfig {
-    /// Use OHUI.
-    pub ohui: bool,
-    /// Create the OHUI thread.
-    pub iothread: Option<String>,
-    /// Confirm related files' path.
-    pub path: String,
+fn get_sock_path(p: &str) -> Result<String> {
+    let path = std::fs::canonicalize(p.clone())
+        .with_context(|| format!("Failed to get real directory path: {:?}", p.clone()))?;
+    if !path.exists() {
+        bail!(
+            "The defined directory {:?} path doesn't exist",
+            path.as_os_str()
+        );
+    }
+    if !path.is_dir() {
+        bail!(
+            "The defined socks-path {:?} is not directory",
+            path.as_os_str()
+        );
+    }
+
+    Ok(path.to_str().unwrap().to_string())
 }
 
 /// GTK and OHUI related configuration.
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Parser, Debug, Clone, Default, Serialize, Deserialize)]
+#[command(no_binary_name(true))]
+
 pub struct DisplayConfig {
-    /// Create the GTK thread.
-    pub gtk: bool,
+    #[arg(long, alias = "classtype", value_parser = ["gtk", "ohui"])]
+    pub display_type: String,
     /// App name if configured.
+    #[arg(long)]
     pub app_name: Option<String>,
     /// Keep the window fill the desktop.
+    #[arg(long, default_value = "off", action = ArgAction::Append, value_parser = parse_bool)]
     pub full_screen: bool,
-    /// Used for OHUI
+    /// Create the OHUI thread.
     #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-    pub ohui_config: OhuiConfig,
+    #[arg(long)]
+    pub iothread: Option<String>,
+    /// Confirm related files' path. Default ui path is "/tmp".
+    #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
+    #[arg(long, alias = "socks-path", default_value = "/tmp/", value_parser = get_sock_path)]
+    pub path: String,
 }
 
 #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
 impl DisplayConfig {
     pub fn get_ui_path(&self) -> String {
-        self.ohui_config.path.clone()
+        self.path.clone()
     }
 }
 
 impl VmConfig {
     pub fn add_display(&mut self, vm_config: &str) -> Result<()> {
-        let mut cmd_parser = CmdParser::new("display");
-        cmd_parser.push("").push("full-screen").push("app-name");
-        #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-        cmd_parser.push("iothread").push("socks-path");
-        cmd_parser.parse(vm_config)?;
-        let mut display_config = DisplayConfig::default();
-        if let Some(str) = cmd_parser.get_value::<String>("")? {
-            match str.as_str() {
-                "gtk" => display_config.gtk = true,
-                #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-                "ohui" => display_config.ohui_config.ohui = true,
-                _ => bail!("Unsupported device: {}", str),
-            }
-        }
-        if let Some(name) = cmd_parser.get_value::<String>("app-name")? {
-            display_config.app_name = Some(name);
-        }
-        if let Some(default) = cmd_parser.get_value::<ExBool>("full-screen")? {
-            display_config.full_screen = default.into();
-        }
-
-        #[cfg(all(target_env = "ohos", feature = "ohui_srv"))]
-        if display_config.ohui_config.ohui {
-            if let Some(iothread) = cmd_parser.get_value::<String>("iothread")? {
-                display_config.ohui_config.iothread = Some(iothread);
-            }
-
-            if let Some(path) = cmd_parser.get_value::<String>("socks-path")? {
-                let path = std::fs::canonicalize(path.clone()).with_context(|| {
-                    format!("Failed to get real directory path: {:?}", path.clone())
-                })?;
-                if !path.exists() {
-                    bail!(
-                        "The defined directory {:?} path doesn't exist",
-                        path.as_os_str()
-                    );
-                }
-                if !path.is_dir() {
-                    bail!(
-                        "The defined socks-path {:?} is not directory",
-                        path.as_os_str()
-                    );
-                }
-                display_config.ohui_config.path = path.to_str().unwrap().to_string();
-            } else {
-                display_config.ohui_config.path = DEFAULT_UI_PATH.to_string();
-            }
-        }
-
+        let display_config =
+            DisplayConfig::try_parse_from(str_slip_to_clap(vm_config, true, false))?;
         self.display = Some(display_config);
         Ok(())
     }
@@ -141,28 +110,28 @@ mod tests {
         let config_line = "gtk";
         assert!(vm_config.add_display(config_line).is_ok());
         let display_config = vm_config.display.unwrap();
-        assert_eq!(display_config.gtk, true);
+        assert_eq!(display_config.display_type, "gtk");
         assert_eq!(display_config.full_screen, false);
 
         let mut vm_config = VmConfig::default();
         let config_line = "gtk,full-screen=on";
         assert!(vm_config.add_display(config_line).is_ok());
         let display_config = vm_config.display.unwrap();
-        assert_eq!(display_config.gtk, true);
+        assert_eq!(display_config.display_type, "gtk");
         assert_eq!(display_config.full_screen, true);
 
         let mut vm_config = VmConfig::default();
         let config_line = "gtk,full-screen=off";
         assert!(vm_config.add_display(config_line).is_ok());
         let display_config = vm_config.display.unwrap();
-        assert_eq!(display_config.gtk, true);
+        assert_eq!(display_config.display_type, "gtk");
         assert_eq!(display_config.full_screen, false);
 
         let mut vm_config = VmConfig::default();
         let config_line = "gtk,app-name=desktopappengine";
         assert!(vm_config.add_display(config_line).is_ok());
         let display_config = vm_config.display.unwrap();
-        assert_eq!(display_config.gtk, true);
+        assert_eq!(display_config.display_type, "gtk");
         assert_eq!(display_config.full_screen, false);
         assert_eq!(
             display_config.app_name,
