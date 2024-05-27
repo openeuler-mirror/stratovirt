b62052fab463b659ecb50ab06df17723118bbcad
li-huachao <lihuachao1@huawei.com>
2024-05-27 19:29:26 +0800

    Balloon: introduce scopes traces.
    Add scopes traces for balloon when handle normal balloon、fpr and
    auto-balloon.

--- virtio/src/device/balloon.rs
+++ virtio/src/device/balloon.rs
@@ -612,6 +612,7 @@ impl BalloonIoHandler {
     /// if `req_type` is `BALLOON_INFLATE_EVENT`, then inflate the balloon, otherwise, deflate the
     /// balloon.
     fn process_balloon_queue(&mut self, req_type: bool) -> Result<()> {
+        trace::trace_scope_start!(process_balloon_queue);
         let queue = if req_type {
             trace::virtio_receive_request("Balloon".to_string(), "to inflate".to_string());
             &self.inf_queue
@@ -648,6 +649,7 @@ impl BalloonIoHandler {
     }
 
     fn reporting_evt_handler(&mut self) -> Result<()> {
+        trace::trace_scope_start!(reporting_evt_handler);
         let queue = self
             .report_queue
             .as_ref()
@@ -682,6 +684,7 @@ impl BalloonIoHandler {
     }
 
     fn auto_msg_evt_handler(&mut self) -> Result<()> {
+        trace::trace_scope_start!(auto_msg_evt_handler);
         let queue = self
             .msg_queue
             .as_ref()
