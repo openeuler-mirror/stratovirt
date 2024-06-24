openeuler-mirror/stratovirt

commit 75ed42cc79e376c4757f907cef1bab2e1e7e43c4

update vmm-sys-util to 0.12.1
Adding bitflags v2.5.0
Updating kvm-bindings v0.6.0 -> v0.7.0
Updating kvm-ioctls v0.15.0 -> v0.16.0
Updating vmm-sys-util v0.11.1 -> v0.12.1

util/src/byte_code.rs
@@ -15,7 +15,7 @@ use std::slice::{from_raw_parts, from_raw_parts_mut};
 
 /// A trait bound defined for types which are safe to convert to a byte slice and
 /// to create from a byte slice.
-pub trait ByteCode: Default + Copy + Send + Sync {
+pub trait ByteCode: Default + Send + Sync {
     /// Return the contents of an object (impl trait `ByteCode`) as a slice of bytes.
     /// the inverse of this function is "from_bytes"
     fn as_bytes(&self) -> &[u8] {

address_space/Cargo.toml
@@ -10,7 +10,7 @@ description = "provide memory management for VM"
 libc = "0.2"
 log = "0.4"
 nix = { version = "0.26.2", default-features = false, features = ["fs", "feature"] }
-vmm-sys-util = "0.11.1"
+vmm-sys-util = "0.12.1"
 arc-swap = "1.6.0"
 thiserror = "1.0"
 anyhow = "1.0"

block_backend/Cargo.toml
@@ -7,7 +7,7 @@ license = "Mulan PSL v2"
 
 [dependencies]
 thiserror = "1.0"
-vmm-sys-util = "0.11.0"
+vmm-sys-util = "0.12.1"
 anyhow = "1.0"
 log = "0.4"
 byteorder = "1.4.3"

boot_loader/Cargo.toml
@@ -8,7 +8,7 @@ license = "Mulan PSL v2"
 [dependencies]
 thiserror = "1.0"
 anyhow = "1.0"
-kvm-bindings = { version = "0.6.0", features = ["fam-wrappers"] }
+kvm-bindings = { version = "0.7.0", features = ["fam-wrappers"] }
 log = "0.4"
 address_space = { path = "../address_space" }
 devices = { path = "../devices" }

chardev_backend/Cargo.toml
@@ -6,7 +6,7 @@ edition = "2021"
 license = "Mulan PSL v2"
 
 [dependencies]
-vmm-sys-util = "0.11.0"
+vmm-sys-util = "0.12.1"
 anyhow = "1.0"
 log = "0.4"
 libc = "0.2"

cpu/Cargo.toml
@@ -9,11 +9,11 @@ description = "CPU emulation"
 [dependencies]
 thiserror = "1.0"
 anyhow = "1.0"
-kvm-bindings = { version = "0.6.0", features = ["fam-wrappers"] }
+kvm-bindings = { version = "0.7.0", features = ["fam-wrappers"] }
 nix = { version = "0.26.2", default-features = false, features = ["fs", "feature"] }
 log = "0.4"
 libc = "0.2"
-vmm-sys-util = "0.11.1"
+vmm-sys-util = "0.12.1"
 machine_manager = { path = "../machine_manager" }
 migration = { path = "../migration" }
 migration_derive = { path = "../migration/migration_derive" }

cpu/src/x86_64/mod.rs
@@ -120,7 +120,7 @@ impl X86CPUTopology {
 /// The state of vCPU's register.
 #[allow(clippy::upper_case_acronyms)]
 #[repr(C)]
-#[derive(Copy, Clone, Desc, ByteCode)]
+#[derive(Desc, ByteCode)]
 #[desc_version(compat_version = "0.1.0")]
 pub struct X86CPUState {
     max_vcpus: u32,
@@ -140,6 +140,34 @@ pub struct X86CPUState {
     pub xsave: Xsave,
     pub xcrs: Xcrs,
     pub debugregs: DebugRegs,
+}
+
+impl Clone for X86CPUState {
+    fn clone(&self) -> Self {
+        let mut xsave: Xsave = Default::default();
+        // we just clone xsave.region, because xsave.extra does not save
+        // valid values and it is not allowed to be cloned.
+        xsave.region = self.xsave.region;
+        Self {
+            max_vcpus: self.max_vcpus,
+            nr_threads: self.nr_threads,
+            nr_cores: self.nr_cores,
+            nr_dies: self.nr_dies,
+            nr_sockets: self.nr_sockets,
+            apic_id: self.apic_id,
+            regs: self.regs,
+            sregs: self.sregs,
+            fpu: self.fpu,
+            mp_state: self.mp_state,
+            lapic: self.lapic,
+            msr_len: self.msr_len,
+            msr_list: self.msr_list,
+            cpu_events: self.cpu_events,
+            xsave,
+            xcrs: self.xcrs,
+            debugregs: self.debugregs,
+        }
+    }
 }
 
 impl X86CPUState {
@@ -181,7 +209,8 @@ impl X86CPUState {
         self.msr_len = locked_cpu_state.msr_len;
         self.msr_list = locked_cpu_state.msr_list;
         self.cpu_events = locked_cpu_state.cpu_events;
-        self.xsave = locked_cpu_state.xsave;
+        self.xsave = Default::default();
+        self.xsave.region = locked_cpu_state.xsave.region;
         self.xcrs = locked_cpu_state.xcrs;
         self.debugregs = locked_cpu_state.debugregs;
     }
@@ -512,11 +541,11 @@ impl StateTransfer for CPU {
     }
 
     fn set_state(&self, state: &[u8]) -> Result<()> {
-        let cpu_state = *X86CPUState::from_bytes(state)
+        let cpu_state = X86CPUState::from_bytes(state)
             .with_context(|| MigrationError::FromBytesError("CPU"))?;
 
         let mut cpu_state_locked = self.arch_cpu.lock().unwrap();
-        *cpu_state_locked = cpu_state;
+        *cpu_state_locked = cpu_state.clone();
 
         Ok(())
     }

devices/Cargo.toml
@@ -14,7 +14,7 @@ log = "0.4"
 serde = { version = "1.0", features = ["derive"] }
 strum = "0.24.1"
 strum_macros = "0.24.3"
-vmm-sys-util = "0.11.1"
+vmm-sys-util = "0.12.1"
 byteorder = "1.4.3"
 drm-fourcc = ">=2.2.0"
 once_cell = "1.18.0"

commit 5aae8db00f4eea7cfde01f12e8058fb101f1d408

fix mst compile error
This error was introduced by commit
75ed42cc79e376c4757f907cef1bab2e1e7e43c4, it deleted the Copy trait of
ByteCode, which will cause errors in mst, so change Copy to Clone should
be better modification.

util/src/byte_code.rs
@@ -15,7 +15,7 @@ use std::slice::{from_raw_parts, from_raw_parts_mut};
 
 /// A trait bound defined for types which are safe to convert to a byte slice and
 /// to create from a byte slice.
-pub trait ByteCode: Default + Send + Sync {
+pub trait ByteCode: Clone + Default + Send + Sync {
     /// Return the contents of an object (impl trait `ByteCode`) as a slice of bytes.
     /// the inverse of this function is "from_bytes"
     fn as_bytes(&self) -> &[u8] {