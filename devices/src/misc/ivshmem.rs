1884a71bd982eb081724856102b526c93a7a7c12
zhanghan <zhanghan64@huawei.com>
2024-08-05 19:21:48 +0800

    scream: support volume sync on OHOS This patch extends ivshmem bar0 and adds msix interrupt support to synchronize volume on OHOS. Scream registers bar0 extension operations to handle bar0 read/write. When the guest changes volume, scream driver writes the changed volume to the specific field of bar0 then scream handler sets the OHOS volume. When OHOS volume changes, OH audio framework calls scream callback and then scream notify the guest via msix vector 0.
    Signed-off-by: 楚影 <lijiahong11@huawei.com>

--- devices/src/misc/ivshmem.rs
+++ devices/src/misc/ivshmem.rs
@@ -12,15 +12,17 @@
 
 use std::sync::{
     atomic::{AtomicU16, Ordering},
-    Arc, Mutex, Weak,
+    Arc, Mutex, RwLock, Weak,
 };
 
 use anyhow::Result;
+use log::error;
 
 use crate::pci::config::{
     PciConfig, RegionType, DEVICE_ID, PCI_CLASS_MEMORY_RAM, PCI_CONFIG_SPACE_SIZE,
     PCI_VENDOR_ID_REDHAT_QUMRANET, REVISION_ID, SUB_CLASS_CODE, VENDOR_ID,
 };
+use crate::pci::msix::init_msix;
 use crate::pci::{le_write_u16, PciBus, PciDevBase, PciDevOps};
 use crate::{convert_bus_ref, Bus, Device, DeviceBase, PCI_BUS};
 use address_space::{GuestAddress, Region, RegionOps};
@@ -34,11 +36,27 @@ const PCI_BAR_MAX_IVSHMEM: u8 = 3;
 
 const IVSHMEM_REG_BAR_SIZE: u64 = 0x100;
 
+const IVSHMEM_BAR0_IRQ_MASK: u64 = 0;
+const IVSHMEM_BAR0_IRQ_STATUS: u64 = 4;
+const IVSHMEM_BAR0_IVPOSITION: u64 = 8;
+const IVSHMEM_BAR0_DOORBELL: u64 = 12;
+
+type Bar0Write = dyn Fn(&[u8], u64) -> bool + Send + Sync;
+type Bar0Read = dyn Fn(&mut [u8], u64) -> bool + Send + Sync;
+
+#[derive(Default)]
+struct Bar0Ops {
+    write: Option<Arc<Bar0Write>>,
+    read: Option<Arc<Bar0Read>>,
+}
+
 /// Intel-VM shared memory device structure.
 pub struct Ivshmem {
     base: PciDevBase,
     dev_id: Arc<AtomicU16>,
     ram_mem_region: Region,
+    vector_nr: u32,
+    bar0_ops: Arc<RwLock<Bar0Ops>>,
 }
 
 impl Ivshmem {
@@ -47,6 +65,7 @@ impl Ivshmem {
         devfn: u8,
         parent_bus: Weak<Mutex<dyn Bus>>,
         ram_mem_region: Region,
+        vector_nr: u32,
     ) -> Self {
         Self {
             base: PciDevBase {
@@ -56,14 +75,45 @@ impl Ivshmem {
             },
             dev_id: Arc::new(AtomicU16::new(0)),
             ram_mem_region,
+            vector_nr,
+            bar0_ops: Arc::new(RwLock::new(Bar0Ops::default())),
         }
     }
 
     fn register_bars(&mut self) -> Result<()> {
-        // Currently, ivshmem uses only the shared memory and does not use interrupt.
-        // Therefore, bar0 read and write callback is not implemented.
-        let reg_read = move |_: &mut [u8], _: GuestAddress, _: u64| -> bool { true };
-        let reg_write = move |_: &[u8], _: GuestAddress, _: u64| -> bool { true };
+        // Currently, ivshmem does not support intx interrupt, ivposition and doorbell.
+        let bar0_ops = self.bar0_ops.clone();
+        let reg_read = move |data: &mut [u8], _: GuestAddress, offset: u64| -> bool {
+            if offset >= IVSHMEM_REG_BAR_SIZE {
+                error!("ivshmem: read offset {} exceeds bar0 size", offset);
+                return true;
+            }
+            match offset {
+                IVSHMEM_BAR0_IRQ_MASK | IVSHMEM_BAR0_IRQ_STATUS | IVSHMEM_BAR0_IVPOSITION => {}
+                _ => {
+                    if let Some(rcb) = bar0_ops.read().unwrap().read.as_ref() {
+                        return rcb(data, offset);
+                    }
+                }
+            }
+            true
+        };
+        let bar0_ops = self.bar0_ops.clone();
+        let reg_write = move |data: &[u8], _: GuestAddress, offset: u64| -> bool {
+            if offset >= IVSHMEM_REG_BAR_SIZE {
+                error!("ivshmem: write offset {} exceeds bar0 size", offset);
+                return true;
+            }
+            match offset {
+                IVSHMEM_BAR0_IRQ_MASK | IVSHMEM_BAR0_IRQ_STATUS | IVSHMEM_BAR0_DOORBELL => {}
+                _ => {
+                    if let Some(wcb) = bar0_ops.read().unwrap().write.as_ref() {
+                        return wcb(data, offset);
+                    }
+                }
+            }
+            true
+        };
         let reg_region_ops = RegionOps {
             read: Arc::new(reg_read),
             write: Arc::new(reg_write),
@@ -73,10 +123,22 @@ impl Ivshmem {
         self.base.config.register_bar(
             0,
             Region::init_io_region(IVSHMEM_REG_BAR_SIZE, reg_region_ops, "IvshmemIo"),
-            RegionType::Mem64Bit,
+            RegionType::Mem32Bit,
             false,
             IVSHMEM_REG_BAR_SIZE,
         )?;
+
+        // bar1: msix
+        if self.vector_nr > 0 {
+            init_msix(
+                &mut self.base,
+                1,
+                self.vector_nr,
+                self.dev_id.clone(),
+                None,
+                None,
+            )?;
+        }
 
         // bar2: ram
         self.base.config.register_bar(
@@ -86,6 +148,22 @@ impl Ivshmem {
             true,
             self.ram_mem_region.size(),
         )
+    }
+
+    pub fn trigger_msix(&self, vector: u16) {
+        if self.vector_nr == 0 {
+            return;
+        }
+        if let Some(msix) = self.base.config.msix.as_ref() {
+            msix.lock()
+                .unwrap()
+                .notify(vector, self.dev_id.load(Ordering::Acquire));
+        }
+    }
+
+    pub fn set_bar0_ops(&mut self, bar0_ops: (Arc<Bar0Write>, Arc<Bar0Read>)) {
+        self.bar0_ops.write().unwrap().write = Some(bar0_ops.0);
+        self.bar0_ops.write().unwrap().read = Some(bar0_ops.1);
     }
 }
 
@@ -117,7 +195,9 @@ impl Device for Ivshmem {
 
         // Attach to the PCI bus.
         let bus = self.parent_bus().unwrap().upgrade().unwrap();
-        let mut locked_bus = bus.lock().unwrap();
+        PCI_BUS!(bus, locked_bus, pci_bus);
+        self.dev_id
+            .store(pci_bus.generate_dev_id(self.base.devfn), Ordering::Release);
         let dev = Arc::new(Mutex::new(self));
         locked_bus.attach_child(dev.lock().unwrap().base.devfn as u64, dev.clone())?;
         Ok(dev)
